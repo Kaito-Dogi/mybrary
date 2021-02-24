59684a3f4bbdb1066b72ca9bfa3b6680521019c8
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-25 04:07:56 +0900

    パーセントの更新改良

--- app/src/main/java/app/doggy/newmybrary/RecordActivity.kt
+++ app/src/main/java/app/doggy/newmybrary/RecordActivity.kt
@@ -1,6 +1,5 @@
 package app.doggy.newmybrary
 
-import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,7 +10,6 @@ import coil.api.load
 import io.realm.Realm
 import io.realm.RealmResults
 import io.realm.Sort
-import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_record.*
 import java.util.*
 
@@ -20,8 +18,6 @@ class RecordActivity : AppCompatActivity() {
     private val realm: Realm by lazy {
         Realm.getDefaultInstance()
     }
-
-    var bookId = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +29,7 @@ class RecordActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        bookId = intent.getStringExtra("bookId") as String
+        val bookId = intent.getStringExtra("bookId") as String
         val book = realm.where(Book::class.java).equalTo("id", bookId).findFirst()
 
         if (book?.imageId == "") {
@@ -47,7 +43,15 @@ class RecordActivity : AppCompatActivity() {
         pageCountText.text = "${book?.pageCount}" + getText(R.string.total_page_text)
         descriptionText.text = book?.description
 
-        val recordList = readAll()
+        val recordList = readAll(bookId)
+
+        if (recordList.size == 0) {
+            realm.executeTransaction {
+                book?.currentPage = 0
+            }
+        } else {
+            updateCurrentPage(bookId)
+        }
 
         val adapter =
                 RecordAdapter(
@@ -67,6 +71,14 @@ class RecordActivity : AppCompatActivity() {
                                         .setPositiveButton(getText(R.string.delete_dialog_positive_button)) { dialog, which ->
                                             Toast.makeText(baseContext, getText(R.string.delete_record_toast_text), Toast.LENGTH_SHORT).show()
                                             deleteRecord(item.id)
+
+                                            if (recordList.size == 0) {
+                                                realm.executeTransaction {
+                                                    book?.currentPage = 0
+                                                }
+                                            } else {
+                                                updateCurrentPage(bookId)
+                                            }
                                         }
                                         .setNegativeButton(getText(R.string.delete_dialog_negative_button)) { dialog, which ->
                                         }
@@ -115,21 +127,10 @@ class RecordActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        val records = realm.where(Record::class.java).equalTo("bookId", bookId).findAll()
-        if (records.size != 0) {
-            updateCurrentPage(bookId)
-        } else {
-            realm.executeTransaction {
-                val book = realm.where(Book::class.java).equalTo("id", bookId).findFirst()
-                        ?: return@executeTransaction
-                book.currentPage = 0
-            }
-        }
         realm.close()
-        bookId = ""
     }
 
-    private fun readAll(): RealmResults<Record> {
+    private fun readAll(bookId: String): RealmResults<Record> {
         return realm.where(Record::class.java)
             .equalTo("bookId", bookId)
             .findAll()
