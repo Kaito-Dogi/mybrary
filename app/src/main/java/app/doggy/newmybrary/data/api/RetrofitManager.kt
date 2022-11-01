3cacbac194ce633a8af7332c58bb36daccf635c7
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-02 02:53:12 +0900

    fix: Retrofit で Service をインスタンス化

--- app/src/main/java/app/doggy/newmybrary/data/api/RetrofitManager.kt
+++ app/src/main/java/app/doggy/newmybrary/data/api/RetrofitManager.kt
@@ -1,6 +1,5 @@
 package app.doggy.newmybrary.data.api
 
-import app.doggy.newmybrary.data.api.service.BookService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.util.concurrent.TimeUnit
@@ -8,7 +7,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
 
 object RetrofitManager {
   private val moshi = Moshi.Builder()
@@ -21,11 +19,9 @@ object RetrofitManager {
     .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
     .build()
 
-  private val retrofit = Retrofit.Builder()
+  val retrofit = Retrofit.Builder()
     .baseUrl(BaseUrl.BOOK_BASE_URL)
     .client(okHttpClient)
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .build()
-
-  val bookService = retrofit.create<BookService>()
 }
--- app/src/main/java/app/doggy/newmybrary/data/api/service/BookService.kt
+++ app/src/main/java/app/doggy/newmybrary/data/api/service/BookService.kt
@@ -1,9 +1,18 @@
 package app.doggy.newmybrary.data.api.service
 
+import app.doggy.newmybrary.data.api.RetrofitManager
 import app.doggy.newmybrary.data.api.response.FetchBooksResponse
+import javax.inject.Inject
 import retrofit2.Response
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
+
+class BookApi @Inject constructor(
+  private val retrofitManager: RetrofitManager,
+) {
+  val service = retrofitManager.retrofit.create<BookService>()
+}
 
 interface BookService {
   @GET("volumes")
