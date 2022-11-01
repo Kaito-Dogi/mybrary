6afe89aca6b57deedeb1e1ad3618ae9b83441aa3
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-02 02:53:32 +0900

    fix: BookRepository を修正

--- app/src/main/java/app/doggy/newmybrary/data/repository/interface/BookRepository.kt
+++ app/src/main/java/app/doggy/newmybrary/data/repository/interface/BookRepository.kt
@@ -3,5 +3,5 @@ package app.doggy.newmybrary.data.repository.`interface`
 import app.doggy.newmybrary.domain.model.Book
 
 interface BookRepository {
-  suspend fun fetchBooksByIsbn(): List<Book>
+  suspend fun fetchBooksByIsbn(isbn: String): List<Book>
 }
