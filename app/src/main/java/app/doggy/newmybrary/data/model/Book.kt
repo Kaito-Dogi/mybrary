98acaf15a345de7fe6cd0ae8fad8a2ee83f8baf6
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-02 01:26:41 +0900

    feat: Book を作成

---
+++ app/src/main/java/app/doggy/newmybrary/data/model/Book.kt
@@ -0,0 +1,14 @@
+package app.doggy.newmybrary.data.model
+
+import java.time.LocalDateTime
+
+data class Book(
+  val id: Int,
+  val title: String,
+  val author: String,
+  val description: String,
+  val totalPage: Int,
+  val imageUrl: String,
+  val recordList: List<Diary>,
+  val registeredAt: LocalDateTime,
+)
