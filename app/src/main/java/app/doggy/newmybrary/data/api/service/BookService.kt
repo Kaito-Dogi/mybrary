d540da2de3acd1683f9ab53fb1f9613329bf6c90
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-02 02:15:49 +0900

    feat: BookService を定義

---
+++ app/src/main/java/app/doggy/newmybrary/data/api/service/BookService.kt
@@ -0,0 +1,10 @@
+package app.doggy.newmybrary.data.api.service
+
+import app.doggy.newmybrary.data.api.response.FetchBooksResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BookService {
+  @GET("volumes")
+  suspend fun fetchBookByIsbn(@Query("q") isbn: String): FetchBooksResponse
+}
