77ac71a2927d31463d20c2c78d2470491f418665
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-02 02:16:57 +0900

    feat: fetchBooksByIsbn の返り値の型を変更

--- app/src/main/java/app/doggy/newmybrary/data/api/service/BookService.kt
+++ app/src/main/java/app/doggy/newmybrary/data/api/service/BookService.kt
@@ -1,10 +1,11 @@
 package app.doggy.newmybrary.data.api.service
 
 import app.doggy.newmybrary.data.api.response.FetchBooksResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BookService {
   @GET("volumes")
-  suspend fun fetchBookByIsbn(@Query("q") isbn: String): FetchBooksResponse
+  suspend fun fetchBookByIsbn(@Query("q") isbn: String): Response<FetchBooksResponse>
 }
