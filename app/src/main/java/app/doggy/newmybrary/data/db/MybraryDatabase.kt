0bf6d5886245193ca662b48126865a98127d7212
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-03 00:05:43 +0900

    feat: export scheme を true にした

--- app/src/main/java/app/doggy/newmybrary/data/db/MybraryDatabase.kt
+++ app/src/main/java/app/doggy/newmybrary/data/db/MybraryDatabase.kt
@@ -9,7 +9,7 @@ import app.doggy.newmybrary.data.db.entity.AuthorEntity
 import app.doggy.newmybrary.data.db.entity.BookEntity
 import app.doggy.newmybrary.data.db.entity.DiaryEntity
 
-@Database(entities = [BookEntity::class, DiaryEntity::class, AuthorEntity::class], version = 1)
+@Database(entities = [BookEntity::class, DiaryEntity::class, AuthorEntity::class], version = 1, exportSchema = true)
 abstract class MybraryDatabase : RoomDatabase() {
   abstract fun bookDao(): BookDao
   abstract fun diaryDao(): DiaryDao
