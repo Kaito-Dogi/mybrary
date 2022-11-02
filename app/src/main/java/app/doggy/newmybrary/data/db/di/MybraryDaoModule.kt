030295b1772e3ab23376fc1859f06ff56f11301e
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-03 00:05:23 +0900

    feat: hilt module を定義

---
+++ app/src/main/java/app/doggy/newmybrary/data/db/di/MybraryDaoModule.kt
@@ -0,0 +1,20 @@
+package app.doggy.newmybrary.data.db.di
+
+import app.doggy.newmybrary.data.db.MybraryDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object MybraryDaoModule {
+  @Provides
+  fun provideBookDao(db: MybraryDatabase) = db.bookDao()
+
+  @Provides
+  fun provideDiaryDao(db: MybraryDatabase) = db.diaryDao()
+
+  @Provides
+  fun provideAuthorDao(db: MybraryDatabase) = db.authorDao()
+}
---
+++ app/src/main/java/app/doggy/newmybrary/data/db/di/MybraryDatabaseModule.kt
@@ -0,0 +1,23 @@
+package app.doggy.newmybrary.data.db.di
+
+import android.content.Context
+import androidx.room.Room
+import app.doggy.newmybrary.data.db.MybraryDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object MybraryDatabaseModule {
+  @Singleton
+  @Provides
+  fun provideMybraryDatabase(
+    @ApplicationContext context: Context,
+  ): MybraryDatabase = Room.databaseBuilder(context, MybraryDatabase::class.java, "mybrary_db")
+    .fallbackToDestructiveMigration()
+    .build()
+}
