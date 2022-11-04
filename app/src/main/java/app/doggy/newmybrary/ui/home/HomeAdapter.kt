783baa3051bb98bb264bcb8d55bbaf3562319606
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-05 01:32:57 +0900

    feat: HomeAdapter の実装

--- app/src/main/java/app/doggy/newmybrary/ui/home/HomeAdapter.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/home/HomeAdapter.kt
@@ -2,4 +2,14 @@ package app.doggy.newmybrary.ui.home
 
 import com.xwray.groupie.GroupieAdapter
 
-class HomeAdapter : GroupieAdapter()
+class HomeAdapter : GroupieAdapter() {
+  fun update(uiModelList: List<HomeUiModel>) {
+    update(
+      uiModelList.map { uiModel ->
+        when (uiModel) {
+          is HomeUiModel.BookUiModel -> BookItem(uiModel)
+        }
+      },
+    )
+  }
+}
