8bd0d6629cb1888e2c49484b6f61b8a7390609a1
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-05 23:07:59 +0900

    feat: バリデーションの処理を ViewModel で実装

--- app/src/main/java/app/doggy/newmybrary/ui/book/register/RegisterFragment.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/book/register/RegisterFragment.kt
@@ -6,9 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import app.doggy.newmybrary.R
 import app.doggy.newmybrary.databinding.FragmentRegisterBinding
-import app.doggy.newmybrary.domain.model.Book
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.Date
 
 @AndroidEntryPoint
 class RegisterFragment : Fragment(R.layout.fragment_register) {
@@ -25,25 +23,17 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     _binding = FragmentRegisterBinding.bind(view)
+    setUpButton()
   }
 
   private fun setUpButton() {
     binding.registerButton.setOnClickListener {
       viewModel.onRegisterButtonClick(
         // TODO: booksApiId, imageUrl が存在する場合を考える
-        book = Book(
-          id = 0L,
-          booksApiId = null,
-          title = binding.titleEditText.text.toString(),
-          authors = listOf(
-            binding.authorEditText.text.toString(),
-          ),
-          description = binding.descriptionEditText.text.toString(),
-          totalPage = binding.totalPageEditText.text.toString().toInt(),
-          imageUrl = null,
-          diaryList = listOf(),
-          registeredAt = Date(),
-        ),
+        title = binding.titleEditText.text.toString(),
+        author = binding.authorEditText.text.toString(),
+        description = binding.descriptionEditText.text.toString(),
+        totalPage = binding.totalPageEditText.text.toString(),
       )
     }
   }
--- app/src/main/java/app/doggy/newmybrary/ui/book/register/RegisterViewModel.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/book/register/RegisterViewModel.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.viewModelScope
 import app.doggy.newmybrary.domain.model.Book
 import app.doggy.newmybrary.domain.repository.BookRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.Date
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 @HiltViewModel
@@ -18,14 +20,38 @@ class RegisterViewModel @Inject constructor(
   private val _uiState = MutableStateFlow(RegisterState.initial())
   val uiState: StateFlow<RegisterState> = _uiState.asStateFlow()
 
-  // TODO: バリデーション処理を考える
-  fun onRegisterButtonClick(book: Book) {
+  fun onRegisterButtonClick(
+    title: String,
+    author: String,
+    description: String,
+    totalPage: String,
+  ) {
     viewModelScope.launch {
+      _uiState.update { it.copy(isLoading = true) }
       runCatching {
-        bookRepository.registerBook(book)
+        val isValid = title.isNotBlank() && author.isNotBlank() && totalPage.isNotBlank() && totalPage.first() != '0'
+        if (!isValid) throw IllegalArgumentException()
+
+        bookRepository.registerBook(
+          book = Book(
+            id = 0L,
+            booksApiId = null,
+            title = title,
+            authors = listOf(
+              author,
+            ),
+            description = description,
+            totalPage = totalPage.toInt(),
+            imageUrl = null,
+            diaryList = listOf(),
+            registeredAt = Date(),
+          ),
+        )
       }.onSuccess {
+        _uiState.update { it.copy(isLoading = false) }
         // TODO: 成功した時のイベントを発火する
       }.onFailure {
+        _uiState.update { it.copy(isLoading = false) }
         // TODO: エラーハンドリング
       }
     }
