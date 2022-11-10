33503e7c0b52ca4c4e2301646c2833c206e8c30b
Kaito-Dogi <kendobu0405@gmail.com>
2022-11-11 02:39:02 +0900

    feat: DetailViewModel を作成

--- app/src/main/java/app/doggy/newmybrary/ui/book/detail/DetailFragment.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/book/detail/DetailFragment.kt
@@ -3,14 +3,19 @@ package app.doggy.newmybrary.ui.book.detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import app.doggy.newmybrary.R
 import app.doggy.newmybrary.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
   private var _binding: FragmentDetailBinding? = null
   private val binding: FragmentDetailBinding
     get() = _binding!!
+
+  private val viewModel: DetailViewModel by viewModels()
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/app/doggy/newmybrary/ui/book/detail/DetailViewModel.kt
@@ -0,0 +1,8 @@
+package app.doggy.newmybrary.ui.book.detail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor() : ViewModel()
