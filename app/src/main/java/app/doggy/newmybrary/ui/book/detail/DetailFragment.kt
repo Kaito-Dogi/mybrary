7eba68529d3f2acca704b37340c55172c9b95f9d
Kaito-Dogi <kendobu0405@gmail.com>
2022-12-11 03:09:14 +0900

    feat: 日記記録画面への遷移を実装

--- app/src/main/java/app/doggy/newmybrary/ui/book/detail/DetailFragment.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/book/detail/DetailFragment.kt
@@ -1,5 +1,6 @@
 package app.doggy.newmybrary.ui.book.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -14,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import app.doggy.newmybrary.R
 import app.doggy.newmybrary.databinding.FragmentDetailBinding
 import app.doggy.newmybrary.domain.model.Book
+import app.doggy.newmybrary.legacy.RecordPostActivity
 import coil.load
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,6 +36,7 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
     super.onViewCreated(view, savedInstanceState)
     _binding = FragmentDetailBinding.bind(view)
     setUpToolbar()
+    setUpButton()
     collectUiState()
   }
 
@@ -45,6 +48,14 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
   private fun setUpToolbar() {
     binding.toolBar.setNavigationOnClickListener {
       findNavController().popBackStack()
+    }
+  }
+
+  private fun setUpButton() {
+    binding.recordButton.setOnClickListener {
+      requireActivity().startActivity(
+        Intent(requireContext(), RecordPostActivity::class.java),
+      )
     }
   }
 
