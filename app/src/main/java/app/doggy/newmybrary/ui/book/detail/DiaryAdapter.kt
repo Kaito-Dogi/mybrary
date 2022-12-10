765710b096d3ab8e6730de703f338272213de584
Kaito-Dogi <kendobu0405@gmail.com>
2022-12-11 02:28:40 +0900

    feat: Diary を表示するための Adapter, ViewHolder の実装

---
+++ app/src/main/java/app/doggy/newmybrary/ui/book/detail/DiaryAdapter.kt
@@ -0,0 +1,34 @@
+package app.doggy.newmybrary.ui.book.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import app.doggy.newmybrary.databinding.ItemDiaryBinding
+import app.doggy.newmybrary.domain.model.Diary
+
+class DiaryAdapter(
+  private val totalPage: Int,
+) : ListAdapter<Diary, DiaryViewHolder>(DIFF_UTIL_ITEM_CALLBACK) {
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiaryViewHolder {
+    val binding = ItemDiaryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    return DiaryViewHolder(
+      binding = binding,
+      totalPage = totalPage,
+    )
+  }
+
+  override fun onBindViewHolder(holder: DiaryViewHolder, position: Int) {
+    holder.bind(getItem(position))
+  }
+}
+
+private val DIFF_UTIL_ITEM_CALLBACK = object : DiffUtil.ItemCallback<Diary>() {
+  override fun areContentsTheSame(oldItem: Diary, newItem: Diary): Boolean {
+    return oldItem == newItem
+  }
+
+  override fun areItemsTheSame(oldItem: Diary, newItem: Diary): Boolean {
+    return oldItem.hashCode() == newItem.hashCode()
+  }
+}
---
+++ app/src/main/java/app/doggy/newmybrary/ui/book/detail/DiaryViewHolder.kt
@@ -0,0 +1,26 @@
+package app.doggy.newmybrary.ui.book.detail
+
+import androidx.recyclerview.widget.RecyclerView
+import app.doggy.newmybrary.R
+import app.doggy.newmybrary.databinding.ItemDiaryBinding
+import app.doggy.newmybrary.domain.model.Diary
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class DiaryViewHolder(
+  private val binding: ItemDiaryBinding,
+  private val totalPage: Int,
+) : RecyclerView.ViewHolder(binding.root) {
+  fun bind(diary: Diary) {
+    binding.currentPageAndPercentText.text = binding.root.context.getString(
+      R.string.text_current_page_and_percent_text,
+      diary.currentPage,
+      totalPage,
+    )
+    binding.datetimeText.text = SimpleDateFormat(
+      binding.root.context.getString(R.string.text_datetime_text),
+      Locale.JAPAN,
+    ).format(diary.recordedAt)
+    binding.contentText.text = diary.content
+  }
+}
