edf9b974d48c4effd137e050f0963cf9d762f87b
Kaito-Dogi <kendobu0405@gmail.com>
2022-12-11 02:41:39 +0900

    fix: Diary の表示を調整

--- app/src/main/java/app/doggy/newmybrary/ui/book/detail/DiaryViewHolder.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/book/detail/DiaryViewHolder.kt
@@ -15,7 +15,7 @@ class DiaryViewHolder(
     binding.currentPageAndPercentText.text = binding.root.context.getString(
       R.string.text_current_page_and_percent_text,
       diary.currentPage,
-      totalPage,
+      diary.getPercent(totalPage),
     )
     binding.datetimeText.text = SimpleDateFormat(
       binding.root.context.getString(R.string.text_datetime_text),
