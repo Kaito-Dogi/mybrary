84b808b8b42050bbc917fa0db687025473ff9f75
Kaito-Dogi <kendobu0405@gmail.com>
2022-12-11 04:28:57 +0900

    feat: RecordViewModel で args を受け取れるようにした

--- app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordFragment.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import app.doggy.newmybrary.R
 import app.doggy.newmybrary.databinding.FragmentRecordBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,8 @@ class RecordFragment : Fragment(R.layout.fragment_record) {
     get() = _binding!!
 
   private val viewModel: RecordViewModel by viewModels()
+
+  private val args: RecordFragmentArgs by navArgs()
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordViewModel.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordViewModel.kt
@@ -1,7 +1,15 @@
 package app.doggy.newmybrary.ui.diary.record
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import app.doggy.newmybrary.domain.repository.DiaryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class RecordViewModel : ViewModel()
+class RecordViewModel @Inject constructor(
+  savedStateHandle: SavedStateHandle,
+  private val diaryRepository: DiaryRepository,
+) : ViewModel() {
+  private val args = RecordFragmentArgs.fromSavedStateHandle(savedStateHandle)
+}
