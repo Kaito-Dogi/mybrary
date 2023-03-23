097bcb8b7e1e33b44bc2caf39a27932ed97fbd85
Kaito-Dogi <kendobu0405@gmail.com>
2023-03-24 00:49:27 +0900

    feat: RecordFragment の Toolbar を追加

--- app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordFragment.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import app.doggy.newmybrary.R
 import app.doggy.newmybrary.databinding.FragmentRecordBinding
@@ -22,10 +23,17 @@ class RecordFragment : Fragment(R.layout.fragment_record) {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     _binding = FragmentRecordBinding.bind(view)
+    setUpToolbar()
   }
 
   override fun onDestroyView() {
     super.onDestroyView()
     _binding = null
+  }
+
+  private fun setUpToolbar() {
+    binding.toolBar.setNavigationOnClickListener {
+      findNavController().popBackStack()
+    }
   }
 }
