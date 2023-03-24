3d7b7d58b29156f265650e9b4080bfecfaf0a110
Kaito-Dogi <kendobu0405@gmail.com>
2023-03-24 17:58:18 +0900

    feat: RecordViewModel の実装

--- app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordViewModel.kt
+++ app/src/main/java/app/doggy/newmybrary/ui/diary/record/RecordViewModel.kt
@@ -2,9 +2,18 @@ package app.doggy.newmybrary.ui.diary.record
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.doggy.newmybrary.R
+import app.doggy.newmybrary.domain.model.Diary
 import app.doggy.newmybrary.domain.repository.DiaryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.Date
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class RecordViewModel @Inject constructor(
@@ -12,4 +21,50 @@ class RecordViewModel @Inject constructor(
   private val diaryRepository: DiaryRepository,
 ) : ViewModel() {
   private val args = RecordFragmentArgs.fromSavedStateHandle(savedStateHandle)
+
+  private val _uiState = MutableStateFlow(RecordState())
+  val uiState: StateFlow<RecordState> = _uiState.asStateFlow()
+
+  fun onRecordButtonClick(
+    currentPage: String,
+    content: String,
+  ) {
+    viewModelScope.launch {
+      _uiState.update { it.copy(isLoading = true) }
+      runCatching {
+        val isValid = currentPage.isNotBlank() && currentPage.first() != '0' && content.isNotBlank()
+        if (!isValid) throw IllegalArgumentException()
+
+        diaryRepository.recordDiary(
+          diary = Diary(
+            content = content,
+            currentPage = currentPage.toInt(),
+            recordedAt = Date(),
+          ),
+          bookId = args.bookId,
+        )
+      }.onSuccess {
+        _uiState.update { currentState ->
+          currentState.copy(
+            isLoading = false,
+            isDiaryRecorded = true,
+          )
+        }
+      }.onFailure { throwable ->
+        _uiState.update { currentState ->
+          currentState.copy(
+            isLoading = false,
+            errorMessageRes = when (throwable) {
+              is IllegalArgumentException -> R.string.error_invalid_input
+              else -> R.string.error_failed_to_record_diary
+            },
+          )
+        }
+      }
+    }
+  }
+
+  fun onErrorMessageShown() {
+    _uiState.update { it.copy(errorMessageRes = null) }
+  }
 }
