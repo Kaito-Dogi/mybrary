e0f279f1f5de6f64f6715ffbc52009590ac54b8c
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-07 18:56:41 +0900

    fix: core:data モジュールに data を移した

--- app/src/main/java/app/doggy/mybrary/data/api/BaseUrl.kt
+++
@@ -1,5 +0,0 @@
-package app.doggy.mybrary.data.api
-
-object BaseUrl {
-  const val BOOK_BASE_URL = "https://www.googleapis.com/books/v1/"
-}
---
+++ core/data/src/main/java/app/doggy/mybrary/core/data/repository/DiaryRepositoryImpl.kt
@@ -0,0 +1,35 @@
+package app.doggy.mybrary.core.data.repository
+
+import app.doggy.mybrary.core.data.db.MybraryDatabase
+import app.doggy.mybrary.core.data.db.entity.toDiaryEntity
+import app.doggy.mybrary.core.data.repository.fake.FakeDiaryRepository
+import app.doggy.mybrary.core.domain.model.Diary
+import app.doggy.mybrary.core.domain.repository.DiaryRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+// FIXME: Hilt モジュールの置き場所を考える
+@InstallIn(SingletonComponent::class)
+@Module
+internal interface DiaryRepositoryModule {
+  // fun DiaryRepositoryImpl.bindDiaryRepository(): DiaryRepository
+  @Binds
+  fun FakeDiaryRepository.bindDiaryRepository(): DiaryRepository
+}
+
+class DiaryRepositoryImpl @Inject constructor(
+  private val db: MybraryDatabase,
+) : DiaryRepository {
+  override suspend fun recordDiary(
+    diary: Diary,
+    bookId: Long,
+  ): Boolean = withContext(Dispatchers.IO) {
+    db.diaryDao().insert(diary.toDiaryEntity(bookId))
+    true
+  }
+}
