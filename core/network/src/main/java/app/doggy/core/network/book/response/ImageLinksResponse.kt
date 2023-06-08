10e5c2bbc6d1ea580321dca0303b403994a689b4
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-09 02:05:13 +0900

    feat: モデルへの変換クラスを定義

--- core/network/src/main/java/app/doggy/core/network/book/response/ImageLinksResponse.kt
+++ core/network/src/main/java/app/doggy/core/network/book/response/ImageLinksResponse.kt
@@ -1,6 +1,5 @@
 package app.doggy.core.network.book.response
 
 class ImageLinksResponse(
-  val smallThumbnail: String,
   val thumbnail: String,
 )
--- core/network/src/main/java/app/doggy/core/network/book/response/ItemResponse.kt
+++ core/network/src/main/java/app/doggy/core/network/book/response/ItemResponse.kt
@@ -1,6 +1,29 @@
 package app.doggy.core.network.book.response
 
+import app.doggy.core.common.UnixTime
+import app.doggy.mybrary.core.domain.model.book.Author
+import app.doggy.mybrary.core.domain.model.book.Book
+import app.doggy.mybrary.core.domain.model.book.BookStatus
+import app.doggy.mybrary.core.domain.model.book.BookTotalPage
+
 class ItemResponse(
   val id: String,
-  val volumeInfo: VolumeInfoResponse,
-)
+  private val volumeInfo: VolumeInfoResponse,
+) {
+  fun toBook() = Book(
+    id = 0,
+    name = volumeInfo.title,
+    description = volumeInfo.description ?: "",
+    totalPage = BookTotalPage(volumeInfo.pageCount),
+    imageUrl = volumeInfo.imageLinks.thumbnail,
+    authors = volumeInfo.authors.map {
+      Author(
+        id = 0,
+        name = it,
+      )
+    },
+    registeredAt = UnixTime(0L),
+    isPinned = false,
+    status = BookStatus.WAITING,
+  )
+}
