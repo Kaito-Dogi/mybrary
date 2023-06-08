903b73eb088bec1566fac73dcb3f991d37c5adba
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-09 03:35:37 +0900

    fix: response の戻り値を修正

--- core/network/src/main/java/app/doggy/core/network/book/response/ItemResponse.kt
+++ core/network/src/main/java/app/doggy/core/network/book/response/ItemResponse.kt
@@ -7,23 +7,28 @@ import app.doggy.mybrary.core.domain.model.book.BookStatus
 import app.doggy.mybrary.core.domain.model.book.BookTotalPage
 
 class ItemResponse(
-  val id: String,
   private val volumeInfo: VolumeInfoResponse,
 ) {
-  fun toBook() = Book(
-    id = 0,
-    name = volumeInfo.title,
-    description = volumeInfo.description ?: "",
-    totalPage = BookTotalPage(volumeInfo.pageCount),
-    imageUrl = volumeInfo.imageLinks.thumbnail,
-    authors = volumeInfo.authors.map {
-      Author(
-        id = 0,
-        name = it,
-      )
-    },
-    registeredAt = UnixTime(0L),
-    isPinned = false,
-    status = BookStatus.WAITING,
-  )
+  fun toBook(): Book {
+    val totalPage =
+      if (volumeInfo.pageCount != null && volumeInfo.pageCount > 0) BookTotalPage(volumeInfo.pageCount)
+      else BookTotalPage(1)
+
+    return Book(
+      id = 0,
+      name = volumeInfo.title,
+      description = volumeInfo.description ?: "",
+      totalPage = totalPage,
+      imageUrl = volumeInfo.imageLinks?.thumbnail ?: "",
+      authors = volumeInfo.authors?.map {
+        Author(
+          id = 0,
+          name = it,
+        )
+      } ?: listOf(),
+      registeredAt = UnixTime(0L),
+      isPinned = false,
+      status = BookStatus.WAITING,
+    )
+  }
 }
--- core/network/src/main/java/app/doggy/core/network/book/response/VolumeInfoResponse.kt
+++ core/network/src/main/java/app/doggy/core/network/book/response/VolumeInfoResponse.kt
@@ -2,8 +2,8 @@ package app.doggy.core.network.book.response
 
 class VolumeInfoResponse(
   val title: String,
-  val authors: List<String>,
+  val authors: List<String>?,
   val description: String?,
-  val pageCount: Int,
-  val imageLinks: ImageLinksResponse,
+  val pageCount: Int?,
+  val imageLinks: ImageLinksResponse?,
 )
