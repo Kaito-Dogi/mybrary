7d23a2c09fe951d75e3b1d1a9946dc5248058f15
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-09 02:17:11 +0900

    feat: BookRepository に searchBooksByIsbn を追加

--- core/domain/src/main/java/app/doggy/mybrary/core/domain/repository/BookRepository.kt
+++ core/domain/src/main/java/app/doggy/mybrary/core/domain/repository/BookRepository.kt
@@ -1,13 +1,22 @@
 package app.doggy.mybrary.core.domain.repository
 
-import app.doggy.mybrary.core.domain.model.legacy.Book
+import app.doggy.mybrary.core.domain.model.legacy.Book as LegacyBook
+import app.doggy.mybrary.core.domain.model.book.Book
+import kotlinx.coroutines.flow.Flow
 
 interface BookRepository {
-  suspend fun fetchBooksByIsbn(isbn: String): List<Book>
+  suspend fun searchBooksByIsbn(
+    isbn: String,
+    limit: Int,
+    pageIndex: Int,
+  ): Flow<List<Book>>
 
-  suspend fun getBooks(): List<Book>
+  // TODO: リファクタする
+  suspend fun fetchBooksByIsbn(isbn: String): List<LegacyBook>
 
-  suspend fun getBook(bookId: Long): Book
+  suspend fun getBooks(): List<LegacyBook>
 
-  suspend fun registerBook(book: Book): Boolean
+  suspend fun getBook(bookId: Long): LegacyBook
+
+  suspend fun registerBook(book: LegacyBook): Boolean
 }
