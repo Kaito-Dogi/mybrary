454f13818a2b0c463d77558bbcfc0f9d151131d1
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-10 17:36:46 +0900

    refactor: Api の処理を明示的に IO スレッドで行うようにした

--- core/network/src/main/java/app/doggy/core/network/api/book/BookApiImpl.kt
+++ core/network/src/main/java/app/doggy/core/network/api/book/BookApiImpl.kt
@@ -1,28 +1,33 @@
 package app.doggy.core.network.api.book
 
+import app.doggy.core.common.coroutines.dispatcher.Dispatcher
+import app.doggy.core.common.coroutines.dispatcher.MybraryDispatchers
 import app.doggy.core.common.exception.NetworkException
 import app.doggy.core.network.retrofit.service.BookService
 import app.doggy.mybrary.core.domain.model.book.Book
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 @Singleton
 internal class BookApiImpl @Inject constructor(
   private val bookService: BookService,
+  @Dispatcher(MybraryDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
 ) : BookApi {
 
   override suspend fun searchBooksByIsbn(
     isbn: String,
     limit: Int,
     pageIndex: Int,
-  ): List<Book> {
+  ): List<Book> = withContext(ioDispatcher) {
     val response = bookService.getVolumes(
       q = isbn,
       maxResults = limit,
       startIndex = pageIndex,
     )
 
-    return if (response.isSuccessful) {
+    return@withContext if (response.isSuccessful) {
       response.body()?.items?.map { it.toBook() } ?: listOf()
     } else {
       throw NetworkException(
