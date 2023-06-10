15d7a23929d1faaa17a51cffc145b90e27e53b5b
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-10 19:22:38 +0900

    feat: BookDao を定義

--- core/database/src/main/java/app/doggy/core/database/dao/BookDao.kt
+++ core/database/src/main/java/app/doggy/core/database/dao/BookDao.kt
@@ -1,6 +1,27 @@
 package app.doggy.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Upsert
+import app.doggy.core.database.entity.BookEntity
+import app.doggy.core.database.entity.BookWithAuthors
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface BookDao
+interface BookDao {
+
+  @Upsert
+  suspend fun upsertBook(bookEntity: BookEntity): Long
+
+  @Query("DELETE FROM books WHERE id = :bookId")
+  suspend fun deleteBookById(bookId: Long)
+
+  @Transaction
+  @Query("SELECT * FROM books")
+  fun getAllBooks(): Flow<List<BookWithAuthors>>
+
+  @Transaction
+  @Query("SELECT * FROM books WHERE id = :bookId")
+  fun getBookById(bookId: Long): Flow<List<BookWithAuthors>>
+}
