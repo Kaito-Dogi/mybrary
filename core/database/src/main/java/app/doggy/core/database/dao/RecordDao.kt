6acf2eeb75d1fa02493792c8b2ea1bfce881160f
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-10 20:41:53 +0900

    feat: RecordDao を定義

--- core/database/src/main/java/app/doggy/core/database/dao/RecordDao.kt
+++ core/database/src/main/java/app/doggy/core/database/dao/RecordDao.kt
@@ -1,6 +1,20 @@
 package app.doggy.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import app.doggy.core.database.entity.RecordEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface RecordDao
+interface RecordDao {
+
+  @Upsert
+  suspend fun upsertRecord(recordEntity: RecordEntity): Long
+
+  @Query("DELETE FROM records  WHERE id = :recordId")
+  suspend fun deleteRecordById(recordId: Long)
+
+  @Query("SELECT * FROM records WHERE id = :recordId")
+  fun getRecordById(recordId: Long): Flow<RecordEntity>
+}
