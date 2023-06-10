d0f765b71fab4c14b9d1bad9c015d0c5c7f304a7
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-11 01:26:25 +0900

    feat: 書籍検索の実装

--- core/data/src/main/java/app/doggy/mybrary/core/data/BookRepositoryImpl.kt
+++ core/data/src/main/java/app/doggy/mybrary/core/data/BookRepositoryImpl.kt
@@ -14,6 +14,7 @@ import app.doggy.mybrary.core.domain.repository.BookRepository
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
@@ -75,7 +76,17 @@ class BookRepositoryImpl @Inject constructor(
     bookDao.getBookWithRecordById(bookId = bookId.value)
       .map { it.toBookWithRecord() }
 
-  override fun searchBooksByIsbn(isbn: String, limit: Int, pageIndex: Int): Flow<List<Book>> {
-    TODO("Not yet implemented")
+  override fun searchBooksByIsbn(
+    isbn: String,
+    limit: Int,
+    pageIndex: Int,
+  ): Flow<List<Book>> = flow {
+    emit(
+      bookApi.searchBooksByIsbn(
+        isbn = isbn,
+        limit = limit,
+        pageIndex = pageIndex,
+      ),
+    )
   }
 }
