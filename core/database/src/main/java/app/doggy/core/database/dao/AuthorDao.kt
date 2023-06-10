907bb3d6b911a13e88cef26b8356420e00a13705
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-11 00:50:49 +0900

    feat: AuthorDao での処理を追加

--- core/database/src/main/java/app/doggy/core/database/dao/AuthorDao.kt
+++ core/database/src/main/java/app/doggy/core/database/dao/AuthorDao.kt
@@ -1,12 +1,21 @@
 package app.doggy.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
 import androidx.room.Upsert
 import app.doggy.core.database.entity.AuthorEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AuthorDao {
 
   @Upsert
   suspend fun upsertAuthors(authors: List<AuthorEntity>): List<Long>
+
+  @Delete
+  suspend fun deleteAuthors(authors: List<AuthorEntity>)
+
+  @Query("SELECT * FROM authors WHERE book_id = :bookId")
+  suspend fun getAuthorsByBookId(bookId: Long): Flow<List<AuthorEntity>>
 }
