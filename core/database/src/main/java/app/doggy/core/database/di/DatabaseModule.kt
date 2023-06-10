b15a67cdf6107e32854efd5bfc0ada1986e6c44d
Kaito-Dogi <kendobu0405@gmail.com>
2023-06-10 14:23:58 +0900

    chore: いままでのデータベースを削除

--- core/database/src/main/java/app/doggy/core/database/di/DatabaseModule.kt
+++ core/database/src/main/java/app/doggy/core/database/di/DatabaseModule.kt
@@ -2,7 +2,6 @@ package app.doggy.core.database.di
 
 import android.content.Context
 import androidx.room.Room
-import app.doggy.core.database.MIGRATION_1_2
 import app.doggy.core.database.MybraryDatabase
 import dagger.Module
 import dagger.Provides
@@ -11,15 +10,18 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
-  @Singleton
+
   @Provides
+  @Singleton
   fun provideMybraryDatabase(
     @ApplicationContext context: Context,
-  ): MybraryDatabase = Room.databaseBuilder(context, MybraryDatabase::class.java, "mybrary_db")
-    .fallbackToDestructiveMigration()
-    .addMigrations(MIGRATION_1_2)
+  ): MybraryDatabase = Room.databaseBuilder(
+    context,
+    MybraryDatabase::class.java,
+    "mybrary_db",
+  )
     .build()
 }
