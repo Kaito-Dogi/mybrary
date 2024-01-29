1c865c5e6eaea0a197601e73606fc08c4c416454
Kaito-Dogi <kendobu0405@gmail.com>
2024-01-30 03:45:47 +0900

    style: kts に改行を追加

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -21,13 +21,16 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
+
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
+
   kotlinOptions {
     jvmTarget = "17"
   }
+
   buildFeatures {
     buildConfig = true
   }
