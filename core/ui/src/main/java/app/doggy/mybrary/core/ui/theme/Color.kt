71f0fd2db11b3ed0e4d47878e32e9e2d7f8a38ff
Kaito-Dogi <kendobu0405@gmail.com>
2024-02-07 03:04:29 +0900

    feat: Color を作成

---
+++ core/ui/src/main/java/app/doggy/mybrary/core/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package app.doggy.mybrary.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// TODO: 調整する
+internal val Primary = Color(0xFF0E1117)
+internal val OnPrimary = Color(0xFFFFFFFF)
+internal val PrimaryContainer = Color(0xFF0E1117)
+internal val SurfaceDim = Color(0xFFF0F0F0)
+internal val Surface = Color(0xFFFFFFFF)
+internal val SurfaceContainer = Color(0xFFFAFAFA)
+internal val OnSurface = Color(0xFF0E1117)
+internal val OnSurfaceVariant = Color(0xFFBDBDBD)
+internal val Outline = Color(0xFFBDBDBD)
+internal val InverseSurface = Color(0xFFFFFFFF)
+internal val InverseOnSurface = Color(0xFF0E1117)
