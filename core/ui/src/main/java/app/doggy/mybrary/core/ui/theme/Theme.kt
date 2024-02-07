b9fdf9a012fcb17802d1a3c0e9057adc612a9f39
Kaito-Dogi <kendobu0405@gmail.com>
2024-02-07 12:32:16 +0900

    chore: 暫定的に dynamic color を無効

--- core/ui/src/main/java/app/doggy/mybrary/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/app/doggy/mybrary/core/ui/theme/Theme.kt
@@ -46,7 +46,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun MybraryTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  dynamicColor: Boolean = true,
+  dynamicColor: Boolean = false,
   content: @Composable () -> Unit,
 ) {
   val colorScheme = when {
