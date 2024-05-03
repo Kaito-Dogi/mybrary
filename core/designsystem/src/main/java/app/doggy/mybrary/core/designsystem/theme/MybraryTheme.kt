d6ac2c4592f243ed9f1f40ca5e9faaa0447413c7
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-03 12:20:51 +0900

    feat: ColorScheme を定義

--- core/designsystem/src/main/java/app/doggy/mybrary/core/designsystem/theme/MybraryTheme.kt
+++ core/designsystem/src/main/java/app/doggy/mybrary/core/designsystem/theme/MybraryTheme.kt
@@ -1,47 +1,23 @@
 package app.doggy.mybrary.core.designsystem.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 @Composable
 fun MybraryTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  dynamicColor: Boolean = false,
   content: @Composable () -> Unit,
 ) {
-  val colorScheme = when {
-    dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-      val context = LocalContext.current
-      if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-    }
-
-    darkTheme -> DarkColorScheme
-    else -> LightColorScheme
-  }
-
-  // TODO: ステータスバーの設定が必要か調査する
-  val view = LocalView.current
-  if (!view.isInEditMode) {
-    SideEffect {
-      val window = (view.context as Activity).window
-      window.statusBarColor = colorScheme.primary.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-    }
+  val colorScheme = if (darkTheme) {
+    mybraryDarkColorScheme
+  } else {
+    mybraryLightColorScheme
   }
 
   CompositionLocalProvider(
