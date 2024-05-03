a10b66ee400a17ae31535c7531d718be4b201bb7
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-03 11:06:36 +0900

    chore: MainActivity を修正

--- app/src/main/java/app/doggy/mybrary/MainActivity.kt
+++ app/src/main/java/app/doggy/mybrary/MainActivity.kt
@@ -3,115 +3,17 @@ package app.doggy.mybrary
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import app.doggy.mybrary.core.designsystem.theme.MybraryTheme
-import app.doggy.mybrary.core.domain.repository.MyBookRepository
-import app.doggy.mybrary.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-  private lateinit var binding: ActivityMainBinding
-
-  @Inject
-  lateinit var myBookRepository: MyBookRepository
-
-  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    // binding = ActivityMainBinding.inflate(layoutInflater)
-    // setContentView(binding.root)
-
     setContent {
       MybraryTheme {
-        // FromBookModule()
-
-        val sheetState = rememberModalBottomSheetState()
-        var isModalShown by remember { mutableStateOf(false) }
-        val hostState = remember { SnackbarHostState() }
-        val scope = rememberCoroutineScope()
-        var text by remember { mutableStateOf("") }
-
-        LaunchedEffect(Unit) {
-          scope.launch {
-            myBookRepository.getMyBooks().forEach {
-              println("あああ: ${it.id}")
-            }
-          }
-        }
-
-        Scaffold(
-          snackbarHost = { SnackbarHost(hostState) },
-        ) { paddingValues ->
-          Column(modifier = Modifier.padding(paddingValues)) {
-            Button(onClick = { isModalShown = true }) {
-              Text(text = "Show Bottom Sheet")
-            }
-
-            Button(
-              onClick = {
-                scope.launch {
-                  isModalShown = true
-                  sheetState.hide()
-                  sheetState.show()
-                  sheetState.expand()
-                }
-              },
-            ) {
-              Text(text = "expand")
-            }
-
-            Button(
-              onClick = {
-                scope.launch {
-                  hostState.showSnackbar("Message")
-                }
-              },
-            ) {
-              Text(text = "Show Snackbar")
-            }
-
-            if (isModalShown) {
-              ModalBottomSheet(
-                onDismissRequest = { isModalShown = false },
-                sheetState = sheetState,
-                modifier = Modifier.weight(1f),
-              ) {
-                Text(text = "あああ")
-                TextField(value = text, onValueChange = { text = it })
-                Button(
-                  onClick = {
-                    scope.launch {
-                      sheetState.expand()
-                    }
-                  },
-                ) {
-                  Text(text = "expand")
-                }
-              }
-            }
-          }
-        }
+        Text(text = "app module")
       }
     }
   }
