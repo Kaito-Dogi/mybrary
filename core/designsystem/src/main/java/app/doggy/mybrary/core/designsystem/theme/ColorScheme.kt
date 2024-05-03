698c2dc89ddae5be11ac5fb6352ec1bf9c8d44f4
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-03 11:04:37 +0900

    refactor: Color を ColorScheme に変更

---
+++ core/designsystem/src/main/java/app/doggy/mybrary/core/designsystem/theme/ColorScheme.kt
@@ -0,0 +1,48 @@
+package app.doggy.mybrary.core.designsystem.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+// TODO: 調整する
+internal val DarkColorScheme = darkColorScheme(
+  // primary = Primary,
+  // onPrimary = OnPrimary,
+  // primaryContainer = PrimaryContainer,
+  // background = SurfaceDim,
+  // surface = Surface,
+  // surfaceVariant = SurfaceContainer,
+  // onSurface = OnSurface,
+  // onSurfaceVariant = OnSurfaceVariant,
+  // inverseSurface = InverseSurface,
+  // inverseOnSurface = InverseOnSurface,
+  // outline = Outline,
+)
+
+// TODO: 調整する
+internal val LightColorScheme = lightColorScheme(
+  // primary = Primary,
+  // onPrimary = OnPrimary,
+  // primaryContainer = PrimaryContainer,
+  // background = SurfaceDim,
+  // surface = Surface,
+  // surfaceVariant = SurfaceContainer,
+  // onSurface = OnSurface,
+  // onSurfaceVariant = OnSurfaceVariant,
+  // inverseSurface = InverseSurface,
+  // inverseOnSurface = InverseOnSurface,
+  // outline = Outline,
+)
+
+// TODO: 調整する
+private val Primary = Color(0xFF0E1117)
+private val OnPrimary = Color(0xFFFFFFFF)
+private val PrimaryContainer = Color(0xFF0E1117)
+private val SurfaceDim = Color(0xFFF0F0F0)
+private val Surface = Color(0xFFFFFFFF)
+private val SurfaceContainer = Color(0xFFFAFAFA)
+private val OnSurface = Color(0xFF0E1117)
+private val OnSurfaceVariant = Color(0xFFBDBDBD)
+private val Outline = Color(0xFFBDBDBD)
+private val InverseSurface = Color(0xFFFFFFFF)
+private val InverseOnSurface = Color(0xFF0E1117)
