c15324f6635329bc2f2aad3fad6ffe6f75504b8f
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-03 11:05:18 +0900

    feat: MybraryTheme を定義

--- core/designsystem/src/main/java/app/doggy/mybrary/core/designsystem/theme/MybraryTheme.kt
+++ core/designsystem/src/main/java/app/doggy/mybrary/core/designsystem/theme/MybraryTheme.kt
@@ -3,47 +3,20 @@ package app.doggy.mybrary.core.designsystem.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-
-// TODO: 調整する
-private val DarkColorScheme = darkColorScheme(
-  // primary = Primary,
-  // onPrimary = OnPrimary,
-  // primaryContainer = PrimaryContainer,
-  // background = SurfaceDim,
-  // surface = Surface,
-  // surfaceVariant = SurfaceContainer,
-  // onSurface = OnSurface,
-  // onSurfaceVariant = OnSurfaceVariant,
-  // inverseSurface = InverseSurface,
-  // inverseOnSurface = InverseOnSurface,
-  // outline = Outline,
-)
-
-// TODO: 調整する
-private val LightColorScheme = lightColorScheme(
-  // primary = Primary,
-  // onPrimary = OnPrimary,
-  // primaryContainer = PrimaryContainer,
-  // background = SurfaceDim,
-  // surface = Surface,
-  // surfaceVariant = SurfaceContainer,
-  // onSurface = OnSurface,
-  // onSurfaceVariant = OnSurfaceVariant,
-  // inverseSurface = InverseSurface,
-  // inverseOnSurface = InverseOnSurface,
-  // outline = Outline,
-)
 
 @Composable
 fun MybraryTheme(
@@ -71,9 +44,35 @@ fun MybraryTheme(
     }
   }
 
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = Typography,
-    content = content,
-  )
+  CompositionLocalProvider(
+    LocalSpace provides space,
+  ) {
+    MaterialTheme(
+      colorScheme = colorScheme,
+      typography = Typography,
+      content = content,
+    )
+  }
+}
+
+object MybraryTheme {
+  val colorScheme: ColorScheme
+    @Composable
+    @ReadOnlyComposable
+    get() = MaterialTheme.colorScheme
+
+  val typography: Typography
+    @Composable
+    @ReadOnlyComposable
+    get() = MaterialTheme.typography
+
+  val shapes: Shapes
+    @Composable
+    @ReadOnlyComposable
+    get() = MaterialTheme.shapes
+
+  val space
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalSpace.current
 }
