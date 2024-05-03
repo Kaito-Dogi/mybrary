46883ab9e31b39d63e7ce4e7b9c74325e11cdd60
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-04 00:52:48 +0900

    feat: デモの画面を設定

--- app/src/main/java/app/doggy/mybrary/MainActivity.kt
+++ app/src/main/java/app/doggy/mybrary/MainActivity.kt
@@ -2,8 +2,14 @@ package app.doggy.mybrary
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import app.doggy.mybrary.core.designsystem.theme.MybraryTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,9 +17,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
     setContent {
       MybraryTheme {
-        Text(text = "app module")
+        Surface {
+          Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+          ) {
+            Text(
+              text = "Mybrary",
+              style = MybraryTheme.typography.displayMedium,
+            )
+          }
+        }
       }
     }
   }
