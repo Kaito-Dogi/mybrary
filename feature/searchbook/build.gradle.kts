8509b0c37dd199ab050183ea4ae89f93a2a8f99d
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-05 02:45:00 +0900

    feat: searchbook モジュールを追加

---
+++ feature/searchbook/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+  alias(libs.plugins.android.library)
+  alias(libs.plugins.kotlin.android)
+}
+
+android {
+  namespace = "app.kaito_dogi.feature.searchbook"
+  compileSdk = 34
+
+  defaultConfig {
+    minSdk = 24
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+          getDefaultProguardFile("proguard-android-optimize.txt"),
+          "proguard-rules.pro",
+      )
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+}
+
+dependencies {
+  implementation(project(":core:domain"))
+  implementation(project(":core:designsystem"))
+
+  // Jetpack Compose
+  val composeBom = platform(libs.androidx.compose.bom)
+  implementation(composeBom)
+  androidTestImplementation(composeBom)
+  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  debugImplementation(libs.androidx.compose.ui.tooling)
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.navigation.compose)
+
+  // Testing
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.test.ext.junit.ktx)
+  androidTestImplementation(libs.androidx.test.espresso.core)
+}
