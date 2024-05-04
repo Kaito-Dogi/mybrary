d59d292a1b4afb42c80731a94c0780345bfa469b
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-05 02:12:21 +0900

    feat: mybookdetail モジュールを追加

---
+++ feature/mybookdetail/src/test/java/app/kaito_dogi/feature/mybookdetail/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package app.kaito_dogi.feature.mybookdetail
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
