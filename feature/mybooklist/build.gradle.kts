29639e786f933b42ed7a7a372303b3f834960b9a
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-06 03:13:13 +0900

    chore: lifecycle-runtime-compose への依存を追加

--- feature/mybooklist/build.gradle.kts
+++ feature/mybooklist/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.ui.tooling)
   implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.navigation.compose)
 
   implementation(libs.coil.compose)
