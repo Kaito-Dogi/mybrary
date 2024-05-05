2e4ef09df90caab0bd957dd9d1121bbb743a5361
Kaito-Dogi <kendobu0405@gmail.com>
2024-05-06 02:55:14 +0900

    chore: Preview 用に ui-tooling を追加

--- feature/mybookdetail/build.gradle.kts
+++ feature/mybookdetail/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
   implementation(composeBom)
   androidTestImplementation(composeBom)
   implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.ui.tooling)
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.navigation.compose)
 
