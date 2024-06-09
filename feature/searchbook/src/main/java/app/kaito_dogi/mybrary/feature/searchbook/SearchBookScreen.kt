0839f7547cc3f815427236086343186bbc65d9d6
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-10 03:14:52 +0900

    refactor: SearchBook で hiltViewModel を使用

--- feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/SearchBookScreen.kt
+++ feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/SearchBookScreen.kt
@@ -1,11 +1,11 @@
 package app.kaito_dogi.mybrary.feature.searchbook
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 internal fun SearchBookScreen(
-  viewModel: SearchBookViewModel = viewModel(),
+  viewModel: SearchBookViewModel = hiltViewModel(),
 ) {
   SearchBookPage()
 }
