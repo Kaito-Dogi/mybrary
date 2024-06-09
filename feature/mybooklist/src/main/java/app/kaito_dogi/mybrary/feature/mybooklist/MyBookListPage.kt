91a3e566f425d21564bdd2ee41f6dc00dd7897a2
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-10 02:31:06 +0900

    refactor: Scaffold を使用

--- feature/mybooklist/src/main/java/app/kaito_dogi/mybrary/feature/mybooklist/MyBookListPage.kt
+++ feature/mybooklist/src/main/java/app/kaito_dogi/mybrary/feature/mybooklist/MyBookListPage.kt
@@ -2,12 +2,8 @@ package app.kaito_dogi.mybrary.feature.mybooklist
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -15,14 +11,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
 import app.kaito_dogi.mybrary.core.domain.model.MyBook
 import app.kaito_dogi.mybrary.feature.mybooklist.component.MyBookCard
-
 
 @Composable
 internal fun MyBookListPage(
@@ -30,15 +25,25 @@ internal fun MyBookListPage(
   onAdditionClick: () -> Unit,
   onMyBookClick: (MyBook) -> Unit,
 ) {
-  Box(
+  Scaffold(
     modifier = Modifier
       .fillMaxSize()
       .background(MybraryTheme.colorScheme.background)
       .padding(horizontal = MybraryTheme.space.md),
-  ) {
+    floatingActionButton = {
+      FloatingActionButton(
+        onClick = onAdditionClick,
+      ) {
+        Icon(
+          imageVector = Icons.Default.Add,
+          contentDescription = "書籍検索画面に遷移",
+        )
+      }
+    },
+  ) { innerPadding ->
     LazyVerticalGrid(
       columns = GridCells.Fixed(uiState.numberOfColumns),
-      contentPadding = WindowInsets.systemBars.asPaddingValues(),
+      contentPadding = innerPadding,
       verticalArrangement = Arrangement.spacedBy(MybraryTheme.space.sm),
       horizontalArrangement = Arrangement.spacedBy(MybraryTheme.space.sm),
     ) {
@@ -51,18 +56,6 @@ internal fun MyBookListPage(
           onClick = onMyBookClick,
         )
       }
-    }
-
-    FloatingActionButton(
-      onClick = onAdditionClick,
-      modifier = Modifier
-        .padding(MybraryTheme.space.md)
-        .align(Alignment.BottomEnd),
-    ) {
-      Icon(
-        imageVector = Icons.Default.Add,
-        contentDescription = "書籍検索画面に遷移",
-      )
     }
   }
 }
