00a982c77790797b065b674bb3ab1a213c3e2025
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-22 01:35:57 +0900

    feat: MockBookRepo を修正

--- core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockMyBookRepository.kt
+++ core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockMyBookRepository.kt
@@ -1,24 +1,34 @@
 package app.kaito_dogi.mybrary.core.data.repository
 
-import app.kaito_dogi.mybrary.core.data.model.dummyMyBooks
+import app.kaito_dogi.mybrary.core.common.model.Url
 import app.kaito_dogi.mybrary.core.domain.model.MyBook
 import app.kaito_dogi.mybrary.core.domain.model.MyBookId
 import app.kaito_dogi.mybrary.core.domain.repository.MyBookRepository
 import javax.inject.Inject
+import javax.inject.Singleton
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
+@Singleton
 internal class MockMyBookRepository @Inject constructor() : MyBookRepository {
+  private val mockMyBookList = MutableStateFlow(MockMyBookList)
+
   override suspend fun getMyBooks(): List<MyBook> {
     delay(1_000)
-    return dummyMyBooks
+
+    return mockMyBookList.value
   }
 
   override suspend fun getMyBook(
     myBookId: MyBookId,
   ): MyBook {
-    return dummyMyBooks.first { it.id == myBookId }
+    delay(1_000)
+
+    return mockMyBookList.value.first { it.id == myBookId }
   }
 
+  // TODO: 実装
   override suspend fun registerBook(
     externalBookId: String,
   ): Boolean {
@@ -28,24 +38,97 @@ internal class MockMyBookRepository @Inject constructor() : MyBookRepository {
   override suspend fun pinBook(
     myBookId: MyBookId,
   ): MyBook {
-    return dummyMyBooks
-      .first { it.id == myBookId }
-      .copy(isPinned = true)
+    delay(1_000)
+
+    val myBook = mockMyBookList.value.first { it.id == myBookId }
+    val pinedMyBook = myBook.copy(isPinned = true)
+    val newMyBookList = mockMyBookList.value.map {
+      if (it.id == myBookId) pinedMyBook else it
+    }
+    mockMyBookList.update { newMyBookList }
+
+    return pinedMyBook
   }
 
   override suspend fun makeBookFavorite(
     myBookId: MyBookId,
   ): MyBook {
-    return dummyMyBooks
-      .first { it.id == myBookId }
-      .copy(isFavorite = true)
+    delay(1_000)
+
+    val myBook = mockMyBookList.value.first { it.id == myBookId }
+    val madeFavoriteMyBook = myBook.copy(isFavorite = true)
+    val newMyBookList = mockMyBookList.value.map {
+      if (it.id == myBookId) madeFavoriteMyBook else it
+    }
+    mockMyBookList.update { newMyBookList }
+
+    return madeFavoriteMyBook
+  }
+
+  override suspend fun removeMyBookFromFavorites(myBookId: MyBookId): MyBook {
+    delay(1_000)
+
+    val myBook = mockMyBookList.value.first { it.id == myBookId }
+    val removedMyBook = myBook.copy(isFavorite = false)
+    val newMyBookList = mockMyBookList.value.map {
+      if (it.id == myBookId) removedMyBook else it
+    }
+    mockMyBookList.update { newMyBookList }
+
+    return removedMyBook
   }
 
   override suspend fun archiveBook(
     myBookId: MyBookId,
   ): MyBook {
-    return dummyMyBooks
-      .first { it.id == myBookId }
-      .copy(isArchived = true)
+    delay(1_000)
+
+    val myBook = mockMyBookList.value.first { it.id == myBookId }
+    val archivedMyBook = myBook.copy(isFavorite = true)
+    val newMyBookList = mockMyBookList.value.map {
+      if (it.id == myBookId) archivedMyBook else it
+    }
+    mockMyBookList.update { newMyBookList }
+
+    return archivedMyBook
   }
+}
+
+private val MockMyBookList = List(10) {
+  MyBook(
+    id = MyBookId(it.toLong()),
+    externalId = "externalId$it",
+    title = when (it % 7) {
+      0 -> "プリンシプル オブ プログラミング3年目までに身につけたい一生役立つ101の原理原則"
+      1 -> "ハッカーと画家"
+      2 -> "オブジェクト指向UIデザイン使いやすいソフトウェアの原理"
+      3 -> "ユースケース駆動開発実践ガイド"
+      4 -> "Clean Architecture　達人に学ぶソフトウェアの構造と設計"
+      5 -> "Kotlinイン・アクション"
+      else -> "タイトル"
+    },
+    authors = when (it % 7) {
+      0 -> "上田勲"
+      1 -> ""
+      2 -> "ソシオメディア, 上野学, 藤井幸多"
+      3 -> "ダグ・ローゼンバーグ, マット・ステファン"
+      4 -> "Ｒｏｂｅｒｔ　Ｃ．Ｍａｒｔｉｎ"
+      5 -> "ＤｍｉｔｒｙＪｅｍｅｒｏｖ, ＳｖｅｔｌａｎａＩｓａｋｏｖａ, 長澤太郎, 藤原聖, 山本純平, ｙｙ＿ｙａｎｋ"
+      else -> "著者"
+    },
+    imageUrl = Url.Image(
+      when (it % 7) {
+        0 -> "https://books.google.com/books/content?id=RuKoDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        1 -> "https://books.google.com/books/content?id=SinFRfuTH7IC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        2 -> "https://books.google.com/books/content?id=1FGpzQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        3 -> "https://books.google.com/books/content?id=IUp4CwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        4 -> "https://books.google.com/books/content?id=1f9lDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        5 -> "https://books.google.com/books/content?id=4TKQswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        else -> ""
+      },
+    ),
+    isPinned = false,
+    isFavorite = false,
+    isArchived = false,
+  )
 }
--- core/domain/src/main/java/app/kaito_dogi/mybrary/core/domain/repository/MyBookRepository.kt
+++ core/domain/src/main/java/app/kaito_dogi/mybrary/core/domain/repository/MyBookRepository.kt
@@ -5,9 +5,16 @@ import app.kaito_dogi.mybrary.core.domain.model.MyBookId
 
 interface MyBookRepository {
   suspend fun getMyBooks(): List<MyBook>
+
   suspend fun getMyBook(myBookId: MyBookId): MyBook
+
   suspend fun registerBook(externalBookId: String): Boolean
+
   suspend fun pinBook(myBookId: MyBookId): MyBook
+
   suspend fun makeBookFavorite(myBookId: MyBookId): MyBook
+
+  suspend fun removeMyBookFromFavorites(myBookId: MyBookId): MyBook
+
   suspend fun archiveBook(myBookId: MyBookId): MyBook
 }
