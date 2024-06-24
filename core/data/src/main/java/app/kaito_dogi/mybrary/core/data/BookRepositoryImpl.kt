522edb08bf05af7d6bf71fa758197f070aee2fcc
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-25 03:08:13 +0900

    fix: BookRepository を修正

--- core/data/src/main/java/app/kaito_dogi/mybrary/core/data/BookRepositoryImpl.kt
+++ core/data/src/main/java/app/kaito_dogi/mybrary/core/data/BookRepositoryImpl.kt
@@ -2,20 +2,12 @@ package app.kaito_dogi.mybrary.core.data
 
 import app.kaito_dogi.mybrary.core.domain.model.Book
 import app.kaito_dogi.mybrary.core.domain.repository.BookRepository
-import app.kaito_dogi.mybrary.core.domain.repository.OrderType
-import app.kaito_dogi.mybrary.core.domain.repository.PrintType
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 internal class BookRepositoryImpl @Inject constructor() : BookRepository {
-  override suspend fun searchBooks(
-    keyword: String,
-    maxResults: Int,
-    startIndex: Int,
-    orderBy: OrderType,
-    printType: PrintType,
-  ): List<Book> {
+  override suspend fun getBook(id: Book) {
     TODO("Not yet implemented")
   }
 }
