0d6d138cabf04e5974fbc18aa8e7b1e07c5f2197
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-26 18:05:43 +0900

    feat: 検索文言を UiState に追加

--- feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/SearchBookUiState.kt
+++ feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/SearchBookUiState.kt
@@ -6,10 +6,12 @@ import app.kaito_dogi.mybrary.core.domain.model.SearchResultBook
 @Immutable
 data class SearchBookUiState(
   val searchResults: List<SearchResultBook>?,
+  val searchQuery: String,
 ) {
   companion object {
     val InitialValue = SearchBookUiState(
       searchResults = null,
+      searchQuery = "",
     )
   }
 }
