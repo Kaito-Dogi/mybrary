a9f9063ab896a6026e5b08a91ad3069e868ab7b8
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-26 22:40:04 +0900

    style: Box を削除して LazyVerticalGrid に contentPadding を適用

--- feature/mybooklist/src/main/java/app/kaito_dogi/mybrary/feature/mybooklist/MyBookListScreen.kt
+++ feature/mybooklist/src/main/java/app/kaito_dogi/mybrary/feature/mybooklist/MyBookListScreen.kt
@@ -1,10 +1,10 @@
 package app.kaito_dogi.mybrary.feature.mybooklist
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -15,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
 import app.kaito_dogi.mybrary.core.domain.model.MyBook
@@ -40,36 +41,36 @@ internal fun MyBookListScreen(
       }
     },
   ) { innerPadding ->
-    Box(
-      modifier = Modifier
-        .fillMaxSize()
-        .padding(innerPadding),
+    val layoutDirection = LocalLayoutDirection.current
+    LazyVerticalGrid(
+      columns = GridCells.Fixed(uiState.numberOfColumns),
+      modifier = Modifier.fillMaxSize(),
+      contentPadding = PaddingValues(
+        start = innerPadding.calculateStartPadding(layoutDirection) + MybraryTheme.space.md,
+        top = innerPadding.calculateTopPadding() + MybraryTheme.space.md,
+        end = innerPadding.calculateEndPadding(layoutDirection) + MybraryTheme.space.md,
+        bottom = innerPadding.calculateBottomPadding(),
+      ),
+      verticalArrangement = Arrangement.spacedBy(MybraryTheme.space.sm),
+      horizontalArrangement = Arrangement.spacedBy(MybraryTheme.space.sm),
     ) {
-      LazyVerticalGrid(
-        columns = GridCells.Fixed(uiState.numberOfColumns),
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(horizontal = MybraryTheme.space.md),
-        verticalArrangement = Arrangement.spacedBy(MybraryTheme.space.sm),
-        horizontalArrangement = Arrangement.spacedBy(MybraryTheme.space.sm),
-      ) {
-        if (uiState.myBookList == null) {
-          // スケルトン表示
-          items(
-            count = 4,
-            key = { "MyBookCellSkeleton$it" },
-          ) {
-            MyBookCellSkeleton()
-          }
-        } else {
-          items(
-            items = uiState.myBookList,
-            key = { it.id.value },
-          ) {
-            MyBookCell(
-              myBook = it,
-              onClick = onMyBookClick,
-            )
-          }
+      if (uiState.myBookList == null) {
+        // スケルトン表示
+        items(
+          count = 4,
+          key = { "MyBookCellSkeleton$it" },
+        ) {
+          MyBookCellSkeleton()
+        }
+      } else {
+        items(
+          items = uiState.myBookList,
+          key = { it.id.value },
+        ) {
+          MyBookCell(
+            myBook = it,
+            onClick = onMyBookClick,
+          )
         }
       }
     }
