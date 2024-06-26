407b5cc9d8fee1c2f5d283d988d62cd10ad1039a
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-26 19:28:52 +0900

    style: skeleton のサイズを修正

--- feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/component/SearchResultBookRowSkeleton.kt
+++ feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/component/SearchResultBookRowSkeleton.kt
@@ -1,12 +1,15 @@
 package app.kaito_dogi.mybrary.feature.searchbook.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,7 +17,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
+import app.kaito_dogi.mybrary.core.ui.component.BookAspectRatio
 import app.kaito_dogi.mybrary.core.ui.component.SkeletonBox
 
 @Composable
@@ -29,17 +34,20 @@ fun SearchResultBookRowSkeleton(
     Row(
       modifier = Modifier
         .padding(MybraryTheme.space.md)
-        .height(IntrinsicSize.Min)
-        .fillMaxWidth(),
+        .fillMaxWidth()
+        .height(IntrinsicSize.Min),
     ) {
+      Box(
+        modifier = Modifier
+          .aspectRatio(BookAspectRatio)
+          .width(72.dp),
+      )
       Column(
         verticalArrangement = Arrangement.spacedBy(MybraryTheme.space.xxs),
       ) {
         Text(
           text = "あ\nあ",
-          modifier = Modifier
-            .fillMaxWidth()
-            .weight(1f),
+          modifier = Modifier.fillMaxWidth(),
           color = Color.Transparent,
           overflow = TextOverflow.Ellipsis,
           maxLines = 2,
