3f92e77c3ca6fa00c3d70996edfd6f7bd71a9b31
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-26 17:50:53 +0900

    feat: BottomAppBar を表示

--- feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/SearchBookScreen.kt
+++ feature/searchbook/src/main/java/app/kaito_dogi/mybrary/feature/searchbook/SearchBookScreen.kt
@@ -3,8 +3,11 @@ package app.kaito_dogi.mybrary.feature.searchbook
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
@@ -12,13 +15,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
+import app.kaito_dogi.mybrary.feature.searchbook.component.SearchBookBottomAppBar
 import app.kaito_dogi.mybrary.feature.searchbook.component.SearchResultBookRow
 
 @Composable
 internal fun SearchBookScreen(
   uiState: SearchBookUiState,
 ) {
-  Scaffold { innerPadding ->
+  Scaffold(
+    bottomBar = {
+      SearchBookBottomAppBar(
+        value = "",
+        onValueChange = {},
+        onBarcodeScannerClick = { println("あああ: barcode_scanner") },
+        modifier = Modifier.windowInsetsPadding(WindowInsets.ime),
+      )
+    },
+  ) { innerPadding ->
     Box(
       modifier = Modifier
         .fillMaxSize()
