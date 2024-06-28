7541f141b8c62b021c0ff43b470284508ce02a67
Kaito-Dogi <kendobu0405@gmail.com>
2024-06-29 01:59:06 +0900

    feat: :core:database:network モジュールを作成

---
+++ core/database/network/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+  alias(libs.plugins.androidLibrary)
+  alias(libs.plugins.hilt)
+  alias(libs.plugins.kotlinAndroid)
+  id("kotlin-kapt")
+}
+
+android {
+  namespace = "app.kaito_dogi.mybrary.core.database.network"
+  compileSdk = libs.versions.compileSdk.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.minSdk.get().toInt()
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro",
+      )
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  kotlinOptions {
+    jvmTarget = libs.versions.jvmTarget.get()
+  }
+}
+
+dependencies {
+  implementation(libs.hiltAndroid)
+
+  kapt(libs.hiltCompiler)
+}
