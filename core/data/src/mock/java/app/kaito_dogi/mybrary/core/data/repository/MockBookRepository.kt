e63136f68b09baabf936c4b23b6a80db7bab8d27
Kaito-Dogi <kendobu0405@gmail.com>
2024-07-01 02:11:44 +0900

    refactor: Book の nullable を整理

--- core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockBookRepository.kt
+++ core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockBookRepository.kt
@@ -1,13 +1,13 @@
 package app.kaito_dogi.mybrary.core.data.repository
 
-import app.kaito_dogi.mybrary.core.domain.model.Book
+import app.kaito_dogi.mybrary.core.domain.model.BookId
 import app.kaito_dogi.mybrary.core.domain.repository.BookRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 internal class MockBookRepository @Inject constructor() : BookRepository {
-  override suspend fun getBook(id: Book) {
+  override suspend fun getBook(id: BookId) {
     TODO("Not yet implemented")
   }
 }
