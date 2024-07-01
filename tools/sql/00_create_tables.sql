1bf5f2c7cd4290afaff3b9380f5c71eaa707008c
Kaito-Dogi <kendobu0405@gmail.com>
2024-07-02 08:27:18 +0900

    refactor: クエリの修正

--- tools/sql/00_create_tables.sql
+++ tools/sql/00_create_tables.sql
@@ -45,10 +45,10 @@ create table public.memos (
   my_book_id bigint not null references public.my_books(id) on delete cascade,
   content text not null,
   start_page int check (start_page > 0),
-  end_page int check (end_page > start_page),
+  end_page int check (end_page > 0),
   edited_at timestamp,
   published_at timestamp,
-  like_count int check (like_count >= 0),
+  like_count int not null check (like_count >= 0),
   created_at timestamp not null default current_timestamp,
   updated_at timestamp not null default current_timestamp
 );
