e809977f17708c231b676c9edd9da417a89004d7
Kaito-Dogi <kendobu0405@gmail.com>
2024-07-10 03:21:12 +0900

    feat: LoginRepository を作成

---
+++ core/data/src/main/java/app/kaito_dogi/mybrary/core/data/repository/LoginRepositoryModule.kt
@@ -0,0 +1,16 @@
+package app.kaito_dogi.mybrary.core.data.repository
+
+import app.kaito_dogi.mybrary.core.domain.repository.LoginRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface LoginRepositoryModule {
+  @Singleton
+  @Binds
+  fun bindLoginRepository(impl: LoginRepositoryImpl): LoginRepository
+}
