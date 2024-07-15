8fe65e784c3b482453077668a20764c55f5daa3d
Kaito-Dogi <kendobu0405@gmail.com>
2024-07-16 00:43:09 +0900

    fix: policy のエラーを修正

--- tools/sql/05_create_update_policies.sql
+++ tools/sql/05_create_update_policies.sql
@@ -1,13 +1,13 @@
 create policy "Authenticated users can update their own profile."
 on profile for update
 to authenticated
-using ( auth.uid() = user_id );
+using ( auth.uid() = user_id )
 with check ( auth.uid() = user_id );
 
 create policy "Authenticated users can update their own my_book."
 on my_book for update
 to authenticated
-using ( auth.uid() = user_id );
+using ( auth.uid() = user_id )
 with check ( auth.uid() = user_id );
 
 create policy "Authenticated users can update their own memo."
@@ -18,7 +18,7 @@ using (
     select user_id from my_book
     where my_book.id = memo.my_book_id
   )
-);
+)
 with check (
   auth.uid() = (
     select user_id from my_book
