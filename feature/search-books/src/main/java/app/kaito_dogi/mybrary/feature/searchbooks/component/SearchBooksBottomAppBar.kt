53f98b8426be8c910714e362279f26c8b3d816ad
Kaito-Dogi <kendobu0405@gmail.com>
2024-07-15 23:14:45 +0900

    refactor: SearchBooks の文言を string resources で管理

--- feature/search-books/src/main/java/app/kaito_dogi/mybrary/feature/searchbooks/component/SearchBooksBottomAppBar.kt
+++ feature/search-books/src/main/java/app/kaito_dogi/mybrary/feature/searchbooks/component/SearchBooksBottomAppBar.kt
@@ -3,14 +3,13 @@ package app.kaito_dogi.mybrary.feature.searchbooks.component
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import app.kaito_dogi.mybrary.core.designsystem.component.Icon
+import app.kaito_dogi.mybrary.core.designsystem.component.Text
 import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
 import app.kaito_dogi.mybrary.core.ui.R
 
@@ -29,26 +28,29 @@ internal fun SearchBooksBottomAppBar(
       value = value,
       onValueChange = onValueChange,
       modifier = Modifier.weight(1f),
-      placeholder = { Text(text = "検索ワードを入力…") },
+      placeholder = { Text(textResId = R.string.search_books_placeholder_enter_search_keywords) },
       leadingIcon = {
         Icon(
-          painter = painterResource(R.drawable.icon_search),
-          contentDescription = "書籍一覧画面に戻る",
+          iconResId = R.drawable.icon_search,
+          descResId = R.string.search_books_desc_search_for_books,
         )
       },
       keyboardOptions = KeyboardOptions.Default.copy(
         imeAction = ImeAction.Done,
       ),
+      singleLine = true,
     )
-    // TODO: v2 以降で実装する
+
 //    Gap(width = MybraryTheme.space.sm)
+//
+//    // TODO: v2 以降で実装する
 //    FloatingActionButton(
 //      onClick = onBarcodeScannerClick,
 //      elevation = FloatingActionButtonDefaults.elevationZero(),
 //    ) {
 //      Icon(
-//        painter = painterResource(R.drawable.icon_barcode_scanner),
-//        contentDescription = "バーコードを読み取る",
+//        iconResId = R.drawable.icon_barcode_scanner,
+//        descResId = R.string.search_books_desc_scan_book_barcode,
 //      )
 //    }
   }
--- feature/search-books/src/main/java/app/kaito_dogi/mybrary/feature/searchbooks/component/SearchResultBookRowSkeleton.kt
+++ feature/search-books/src/main/java/app/kaito_dogi/mybrary/feature/searchbooks/component/SearchResultBookRowSkeleton.kt
@@ -10,14 +10,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.kaito_dogi.mybrary.core.designsystem.component.SkeletonBox
+import app.kaito_dogi.mybrary.core.designsystem.component.Text
 import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
+import app.kaito_dogi.mybrary.core.ui.R
 import app.kaito_dogi.mybrary.core.ui.component.BookAspectRatio
 
 @Composable
@@ -39,23 +40,26 @@ fun SearchResultBookRowSkeleton(
           .aspectRatio(BookAspectRatio)
           .width(72.dp),
       )
+
       Column(
         verticalArrangement = Arrangement.spacedBy(MybraryTheme.space.xxs),
       ) {
         Text(
-          text = "あ\nあ",
+          textResId = R.string.search_books_text_a_a,
           modifier = Modifier.fillMaxWidth(),
           color = Color.Transparent,
           style = MybraryTheme.typography.titleMedium,
         )
+
         Text(
-          text = "あ",
+          textResId = R.string.search_books_text_a,
           modifier = Modifier.fillMaxWidth(),
           color = Color.Transparent,
           style = MybraryTheme.typography.bodyMedium,
         )
+
         Text(
-          text = "あ",
+          textResId = R.string.search_books_text_a,
           modifier = Modifier.fillMaxWidth(),
           color = Color.Transparent,
           style = MybraryTheme.typography.bodyMedium,
