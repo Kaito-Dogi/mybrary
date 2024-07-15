b2e39bc883b40f06f7bc350f30a52489fdeeb577
Kaito-Dogi <kendobu0405@gmail.com>
2024-07-15 17:50:37 +0900

    feat: resource id を代入できる Icon を作成

---
+++ core/design-system/src/main/java/app/kaito_dogi/mybrary/core/designsystem/component/Icon.kt
@@ -0,0 +1,23 @@
+package app.kaito_dogi.mybrary.core.designsystem.component
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun Icon(
+  @DrawableRes iconResId: Int,
+  @StringRes contentDescriptionResId: Int?,
+  modifier: Modifier = Modifier,
+  tint: Color = LocalContentColor.current,
+) = androidx.compose.material3.Icon(
+  painter = painterResource(id = iconResId),
+  contentDescription = contentDescriptionResId?.let { stringResource(id = it) },
+  modifier = modifier,
+  tint = tint,
+)
