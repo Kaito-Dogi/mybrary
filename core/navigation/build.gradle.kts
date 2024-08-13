dc75f032f058811de43b2dd08f1303477e4c0e87
Kaito-Dogi <kendobu0405@gmail.com>
2024-08-13 17:13:24 +0900

    fix: :core:navigation を android モジュールにした

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,12 +1,46 @@
 plugins {
-  alias(libs.plugins.javaLibrary)
-  alias(libs.plugins.jetbrainsKotlinJvm)
+  alias(libs.plugins.androidLibrary)
+  alias(libs.plugins.kotlinAndroid)
   alias(libs.plugins.serialization)
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_17
-  targetCompatibility = JavaVersion.VERSION_17
+android {
+  namespace = "app.kaito_dogi.mybrary.core.navigation"
+  compileSdk = libs.versions.compileSdk.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.minSdk.get().toInt()
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro",
+      )
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  kotlinOptions {
+    jvmTarget = libs.versions.jvmTarget.get()
+  }
+
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+  }
 }
 
 dependencies {
