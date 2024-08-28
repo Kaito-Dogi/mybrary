8c7228903727d9b109c5a5957e433b7379f042f5
Kaito-Dogi <kendobu0405@gmail.com>
2024-08-29 01:38:20 +0900

    feat: BookRepository を定義

--- core/data/src/main/java/app/kaito_dogi/mybrary/core/data/repository/BookRepositoryImpl.kt
+++ core/data/src/main/java/app/kaito_dogi/mybrary/core/data/repository/BookRepositoryImpl.kt
@@ -1,13 +1,30 @@
 package app.kaito_dogi.mybrary.core.data.repository
 
+import app.kaito_dogi.mybrary.core.api.rakuten.RakutenApi
+import app.kaito_dogi.mybrary.core.domain.model.Book
 import app.kaito_dogi.mybrary.core.domain.model.BookId
 import app.kaito_dogi.mybrary.core.domain.repository.BookRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-internal class BookRepositoryImpl @Inject constructor() : BookRepository {
-  override suspend fun getBook(id: BookId) {
+internal class BookRepositoryImpl @Inject constructor(
+  private val rakutenApi: RakutenApi,
+) : BookRepository {
+  override suspend fun getBook(id: BookId): Book {
+    TODO("Not yet implemented")
+  }
+
+  override suspend fun searchBook(
+    title: String,
+    author: String,
+    publisher: String,
+    size: Int,
+    isbn: String,
+    hits: Int,
+    page: Int,
+    sort: String,
+  ): List<Book> {
     TODO("Not yet implemented")
   }
 }
--- core/domain/src/main/java/app/kaito_dogi/mybrary/core/domain/repository/BookRepository.kt
+++ core/domain/src/main/java/app/kaito_dogi/mybrary/core/domain/repository/BookRepository.kt
@@ -1,7 +1,21 @@
 package app.kaito_dogi.mybrary.core.domain.repository
 
+import app.kaito_dogi.mybrary.core.domain.model.Book
 import app.kaito_dogi.mybrary.core.domain.model.BookId
 
 interface BookRepository {
-  suspend fun getBook(id: BookId)
+  suspend fun getBook(id: BookId): Book
+
+  suspend fun searchBook(
+    title: String,
+    author: String,
+    publisher: String,
+    // FIXME: Enum で指定できるようにする
+    size: Int,
+    isbn: String,
+    hits: Int,
+    page: Int,
+    // FIXME: Enum で指定できるようにする
+    sort: String,
+  ): List<Book>
 }
