81216399caeb1f1b1b0aee01dead0cb23de1425f
Kaito-Dogi <kendobu0405@gmail.com>
2024-08-30 00:20:11 +0900

    feat: Category を追加

---
+++ core/domain/src/main/java/app/kaito_dogi/mybrary/core/domain/model/Category.kt
@@ -0,0 +1,15 @@
+package app.kaito_dogi.mybrary.core.domain.model
+
+enum class Category(val value: Int) {
+  All(value = 0),
+  Hardcover(value = 1),
+  Paperback(value = 2),
+  NewBook(value = 3),
+  CompleteWorks(value = 4),
+  Dictionary(value = 5),
+  IllustratedBook(value = 6),
+  PictureBook(value = 7),
+  CassetteCd(value = 8),
+  Comics(value = 9),
+  MookOthers(value = 10),
+}
