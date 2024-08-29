44ff705a98517ca163b866f8e31f2d131b081d7b
Kaito-Dogi <kendobu0405@gmail.com>
2024-08-30 04:30:43 +0900

    fix: isbn の重複を避けた

--- core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockBookRepository.kt
+++ core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockBookRepository.kt
@@ -47,25 +47,16 @@ private val MockBookList = List(10) { index ->
       1 -> "ピンポン（2）"
       else -> "ピンポン（3）"
     },
-    imageUrl =
-    when (index % 3) {
+    imageUrl = when (index % 3) {
       0 -> Url.Image(value = "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2416/9784091962416.jpg?_ex=512x512")
       1 -> Url.Image(value = "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2423/9784091962423.jpg?_ex=512x512")
       else -> Url.Image(value = "https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2430/9784091962430.jpg?_ex=512x512")
     },
-    isbn = when (index % 5) {
-      0 -> "9784091962416"
-      1 -> "9784091962423"
-      else -> "9784091962430"
-    },
+    isbn = "isbn$index",
     publisher = "小学館",
     authorList = "松本 大洋".toAuthorList(),
     genre = Genre.Paperback,
-    rakutenAffiliateUrl = when (index) {
-      0 -> Url.Affiliate(value = "https://hb.afl.rakuten.co.jp/hgc/g00q072h.iz0yg250.g00q072h.iz0yhd2f/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F11778666%2F")
-      1 -> Url.Affiliate(value = "https://hb.afl.rakuten.co.jp/hgc/g00q072h.iz0yg250.g00q072h.iz0yhd2f/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F11778667%2F")
-      else -> Url.Affiliate(value = "https://hb.afl.rakuten.co.jp/hgc/g00q072h.iz0yg250.g00q072h.iz0yhd2f/?pc=https%3A%2F%2Fbooks.rakuten.co.jp%2Frb%2F11826794%2F")
-    },
+    rakutenAffiliateUrl = Url.Affiliate(value = "rakutenAffiliateUrl$index"),
     amazonAffiliateUrl = null,
   )
 }
