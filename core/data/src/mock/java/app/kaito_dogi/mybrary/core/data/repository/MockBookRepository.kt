7df723ba7f41e8f3a7fd2c28660f426cbc61a88a
Kaito-Dogi <kendobu0405@gmail.com>
2024-08-30 01:33:48 +0900

    feat: searchBook のモックを実装

--- core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockBookRepository.kt
+++ core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/repository/MockBookRepository.kt
@@ -1,15 +1,23 @@
 package app.kaito_dogi.mybrary.core.data.repository
 
-import app.kaito_dogi.mybrary.core.api.rakuten.RakutenApi
+import app.kaito_dogi.mybrary.core.common.coroutines.dispatcher.Dispatcher
+import app.kaito_dogi.mybrary.core.common.coroutines.dispatcher.MybraryDispatchers
+import app.kaito_dogi.mybrary.core.common.model.Url
+import app.kaito_dogi.mybrary.core.domain.model.Author
 import app.kaito_dogi.mybrary.core.domain.model.Book
 import app.kaito_dogi.mybrary.core.domain.model.BookId
+import app.kaito_dogi.mybrary.core.domain.model.Genre
+import app.kaito_dogi.mybrary.core.domain.model.Sort
 import app.kaito_dogi.mybrary.core.domain.repository.BookRepository
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 @Singleton
 internal class MockBookRepository @Inject constructor(
-  private val rakutenApi: RakutenApi,
+  @Dispatcher(MybraryDispatchers.IO) private val dispatcher: CoroutineDispatcher,
 ) : BookRepository {
   override suspend fun getBook(id: BookId): Book {
     TODO("Not yet implemented")
@@ -17,15 +25,26 @@ internal class MockBookRepository @Inject constructor(
 
   override suspend fun searchBook(
     title: String,
-    size: Int,
     isbn: String,
     genre: Genre,
     hits: Int,
     page: Int,
-    sort: String,
-  ): List<Book> {
-    // TODO: MockMyBookRepository と同様に mockBookList を実装する
-    TODO("Not yet implemented")
     sort: Sort,
+  ): List<Book> = withContext(dispatcher) {
+    delay(1_000)
+
+    return@withContext MockMyBookList
   }
+}
+
+private val MockMyBookList = List(10) { index ->
+  Book(
+    id = BookId(value = "$index"),
+    title = "title$index",
+    imageUrl = Url.Image(value = "imageUrl$index"),
+    isbn = "isbn$index",
+    publisher = "publisher$index",
+    authorList = listOf(Author(name = "author$index")),
+    genre = Genre.All,
+  )
 }
