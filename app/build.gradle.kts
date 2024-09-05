1eb5470130db32a376b3986d31164b6b2945fa80
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-05 19:54:51 +0900

    feat: app を my-book-list に依存するようにした

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,6 +135,7 @@ dependencies {
   implementation(project(":core:ui"))
   implementation(project(":feature:my-book"))
   implementation(project(":feature:my-book-detail"))
+  implementation(project(":feature:my-book-list"))
   implementation(project(":feature:search-book"))
   implementation(project(":feature:send-otp"))
   implementation(project(":feature:verify-otp"))
