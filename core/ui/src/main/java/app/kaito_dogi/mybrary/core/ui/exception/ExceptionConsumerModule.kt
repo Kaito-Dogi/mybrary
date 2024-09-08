790a50c7a0a2843199c52b9865c89090951691b1
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-09 01:37:17 +0900

    refactor: Singleton アノテーションを追加

--- core/ui/src/main/java/app/kaito_dogi/mybrary/core/ui/exception/ExceptionConsumerModule.kt
+++ core/ui/src/main/java/app/kaito_dogi/mybrary/core/ui/exception/ExceptionConsumerModule.kt
@@ -4,10 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface ExceptionConsumerModule {
+  @Singleton
   @Binds
   fun bindExceptionConsumer(exceptionHandler: ExceptionHandler): ExceptionConsumer
 }
