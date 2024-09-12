e9c86743074677deb10d28d7c7c175ef52c6f1f0
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-13 00:29:54 +0900

    feat: TopAppBar で navigation icon を渡せるようにした

--- core/design-system/src/main/java/app/kaito_dogi/mybrary/core/designsystem/component/TopAppBar.kt
+++ core/design-system/src/main/java/app/kaito_dogi/mybrary/core/designsystem/component/TopAppBar.kt
@@ -1,7 +1,9 @@
 package app.kaito_dogi.mybrary.core.designsystem.component
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,9 +13,24 @@ import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
 @Composable
 fun TopAppBar(
   @StringRes textResId: Int,
+  @DrawableRes navigationIconResId: Int? = null,
+  @StringRes navigationIconAltResId: Int? = null,
+  onNavigationIconClick: (() -> Unit)? = null,
 ) = androidx.compose.material3.TopAppBar(
   title = {
-    Text(text = stringResource(id = textResId),)
+    Text(text = stringResource(id = textResId))
+  },
+  navigationIcon = {
+    if (navigationIconResId != null && onNavigationIconClick != null) {
+      IconButton(
+        onClick = onNavigationIconClick,
+      ) {
+        Icon(
+          iconResId = navigationIconResId,
+          altResId = navigationIconAltResId,
+        )
+      }
+    }
   },
 )
 
