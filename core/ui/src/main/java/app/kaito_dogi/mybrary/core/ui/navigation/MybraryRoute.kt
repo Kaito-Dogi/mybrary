df4b05a7123f2bb549e1dc08bf47bf40ce49e602
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-17 20:04:32 +0900

    chore: もとの Route を Deprecated にした

--- core/ui/src/main/java/app/kaito_dogi/mybrary/core/ui/navigation/MybraryRoute.kt
+++ core/ui/src/main/java/app/kaito_dogi/mybrary/core/ui/navigation/MybraryRoute.kt
@@ -3,6 +3,7 @@ package app.kaito_dogi.mybrary.core.ui.navigation
 import app.kaito_dogi.mybrary.core.domain.model.MyBook
 import kotlinx.serialization.Serializable
 
+@Deprecated("")
 sealed interface MybraryRoute {
   @Serializable
   data object MyBookList : MybraryRoute
