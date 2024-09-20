8bc604514faa1876c698b3afb9210ed7fc1c7c2d
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-21 04:21:38 +0900

    refactor: Column を Box に修正

--- feature/my-book/src/main/java/app/kaito_dogi/mybrary/feature/mybook/destination/mybooklist/component/MyBookListHeader.kt
+++ feature/my-book/src/main/java/app/kaito_dogi/mybrary/feature/mybook/destination/mybooklist/component/MyBookListHeader.kt
@@ -1,11 +1,11 @@
 package app.kaito_dogi.mybrary.feature.mybook.destination.mybooklist.component
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,11 +18,11 @@ internal fun MyBookListHeader(
   @StringRes titleResId: Int,
   modifier: Modifier = Modifier,
 ) {
-  Column(
+  Box(
     modifier = modifier
       .fillMaxWidth()
       .height(MybraryTheme.dimens.topAppBarHeight - MybraryTheme.spaces.sm * 2),
-    verticalArrangement = Arrangement.Center,
+    contentAlignment = Alignment.CenterStart,
   ) {
     Text(
       text = stringResource(id = titleResId),
