c5c45ffdae292c10286ff16b4b464a40bca408c1
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-21 03:50:44 +0900

    feat: SettingHeader を実装

---
+++ feature/setting/src/main/java/app/kaito_dogi/mybrary/feature/setting/destination/settinglist/component/SettingHeader.kt
@@ -0,0 +1,43 @@
+package app.kaito_dogi.mybrary.feature.setting.destination.settinglist.component
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import app.kaito_dogi.mybrary.core.designsystem.R
+import app.kaito_dogi.mybrary.core.designsystem.component.Text
+import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
+
+@Composable
+internal fun SettingHeader(
+  @StringRes titleResId: Int,
+  modifier: Modifier = Modifier,
+) {
+  Box(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(MybraryTheme.spaces.sm),
+    contentAlignment = Alignment.CenterStart,
+  ) {
+    Text(
+      text = stringResource(titleResId),
+      color = MybraryTheme.colorScheme.onSurfaceVariant,
+      style = MybraryTheme.typography.titleLarge,
+    )
+  }
+}
+
+@Preview
+@Composable
+private fun SettingHeaderPreview() {
+  MybraryTheme {
+    SettingHeader(
+      titleResId = R.string.setting_list_text_account,
+    )
+  }
+}
