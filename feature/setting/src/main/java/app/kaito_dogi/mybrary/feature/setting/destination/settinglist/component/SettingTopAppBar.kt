6de8b8ea68ff70576240090f9ea81fe01c71496e
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-21 05:28:14 +0900

    feat: TopAppBar を実装

---
+++ feature/setting/src/main/java/app/kaito_dogi/mybrary/feature/setting/destination/settinglist/component/SettingTopAppBar.kt
@@ -0,0 +1,34 @@
+package app.kaito_dogi.mybrary.feature.setting.destination.settinglist.component
+
+import androidx.annotation.StringRes
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import app.kaito_dogi.mybrary.core.designsystem.R
+import app.kaito_dogi.mybrary.core.designsystem.component.Text
+import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SettingListTopAppBar(
+  @StringRes titleResId: Int,
+) {
+  TopAppBar(
+    title = {
+      Text(
+        text = stringResource(titleResId),
+        style = MybraryTheme.typography.headlineSmall,
+      )
+    },
+  )
+}
+
+@Preview
+@Composable
+private fun SettingListTopAppBarPreview() {
+  MybraryTheme {
+    SettingListTopAppBar(titleResId = R.string.setting_list_text_setting)
+  }
+}
