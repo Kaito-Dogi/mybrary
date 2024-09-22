cd9b3da398b3d61a7f2c2d97404ccb3203e65648
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-23 03:12:10 +0900

    style: TopAppBar を修正

--- feature/setting/src/main/java/app/kaito_dogi/mybrary/feature/setting/destination/settinglist/component/SettingListTopAppBar.kt
+++ feature/setting/src/main/java/app/kaito_dogi/mybrary/feature/setting/destination/settinglist/component/SettingListTopAppBar.kt
@@ -1,27 +1,32 @@
 package app.kaito_dogi.mybrary.feature.setting.destination.settinglist.component
 
-import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import app.kaito_dogi.mybrary.core.designsystem.R
 import app.kaito_dogi.mybrary.core.designsystem.component.Text
+import app.kaito_dogi.mybrary.core.designsystem.ext.none
 import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SettingListTopAppBar(
-  @StringRes titleResId: Int,
+  modifier: Modifier = Modifier,
 ) {
+  // FIXME: NavigationBarItem で表示できる NavHost の Root の TopAppBar として共通化する
   CenterAlignedTopAppBar(
     title = {
       Text(
-        text = stringResource(titleResId),
+        text = stringResource(R.string.setting_list_text_setting),
         style = MybraryTheme.typography.headlineSmall,
       )
     },
+    modifier = modifier,
+    windowInsets = WindowInsets.none,
   )
 }
 
@@ -29,6 +34,6 @@ internal fun SettingListTopAppBar(
 @Composable
 private fun SettingListTopAppBarPreview() {
   MybraryTheme {
-    SettingListTopAppBar(titleResId = R.string.setting_list_text_setting)
+    SettingListTopAppBar()
   }
 }
