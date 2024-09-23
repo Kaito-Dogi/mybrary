f6e88a21bc5662b326359824b338871e66636741
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-23 23:57:01 +0900

    feat: SettingListViewModel を実装

---
+++ feature/setting/src/main/java/app/kaito_dogi/mybrary/feature/setting/destination/settinglist/SettingListViewModel.kt
@@ -0,0 +1,23 @@
+package app.kaito_dogi.mybrary.feature.setting.destination.settinglist
+
+import androidx.lifecycle.ViewModel
+import app.kaito_dogi.mybrary.core.config.MybraryConfig
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+@HiltViewModel
+internal class SettingListViewModel @Inject constructor(
+  config: MybraryConfig,
+) : ViewModel() {
+  private val _uiState = MutableStateFlow(
+    SettingListUiState.createInitialValue(
+      termsOfUseUrl = config.termsOfUseUrl,
+      privacyPolicyUrl = config.privacyPolicyUrl,
+      rakutenDevelopersUrl = config.rakutenDevelopersUrl,
+      versionName = config.versionName,
+    ),
+  )
+  val uiState = _uiState.asStateFlow()
+}
