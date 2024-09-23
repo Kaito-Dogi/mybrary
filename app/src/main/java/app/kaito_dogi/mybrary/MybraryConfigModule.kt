e82e23ad78b175356eaf0d4c6e8fc6fc05c44cfa
Kaito-Dogi <kendobu0405@gmail.com>
2024-09-23 23:55:39 +0900

    feat: Config に設定画面の Url を追加

--- app/src/main/java/app/kaito_dogi/mybrary/MybraryConfigModule.kt
+++ app/src/main/java/app/kaito_dogi/mybrary/MybraryConfigModule.kt
@@ -1,5 +1,6 @@
 package app.kaito_dogi.mybrary
 
+import app.kaito_dogi.mybrary.core.common.model.Url
 import app.kaito_dogi.mybrary.core.config.MybraryConfig
 import dagger.Module
 import dagger.Provides
@@ -13,10 +14,16 @@ internal object MybraryConfigModule {
   @Singleton
   @Provides
   fun provideMybraryConfig(): MybraryConfig = object : MybraryConfig {
+    override val privacyPolicyUrl: Url.Web
+      get() = TODO("Not yet implemented")
     override val rakutenApplicationId: String = BuildConfig.RAKUTEN_APPLICATION_ID
     override val rakutenAffiliateId: String = BuildConfig.RAKUTEN_AFFILIATE_ID
+    override val rakutenDevelopersUrl: Url.Web
+      get() = TODO("Not yet implemented")
     override val supabaseUrl: String = BuildConfig.SUPABASE_URL
     override val supabaseKey: String = BuildConfig.SUPABASE_KEY
+    override val termsOfUseUrl: Url.Web
+      get() = TODO("Not yet implemented")
     override val versionName: String = BuildConfig.VERSION_NAME
   }
 }
