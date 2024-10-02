eedd7622192e1376462205eac1f4a4da07703aee
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-02 23:38:36 +0900

    feat: SendOtp への遷移を追加

--- feature/auth/src/main/java/app/kaito_dogi/mybrary/feature/auth/destination/sendotp/SendOtpNavigation.kt
+++ feature/auth/src/main/java/app/kaito_dogi/mybrary/feature/auth/destination/sendotp/SendOtpNavigation.kt
@@ -1,8 +1,10 @@
 package app.kaito_dogi.mybrary.feature.auth.destination.sendotp
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import app.kaito_dogi.mybrary.core.ui.navigation.route.AuthRoute
+import app.kaito_dogi.mybrary.core.ui.navigation.route.MybraryRoute
 
 fun NavGraphBuilder.sendOtpScreen(
   onSendOtpComplete: (email: String, AuthRoute.VerifyOtp.Page) -> Unit,
@@ -14,4 +16,10 @@ fun NavGraphBuilder.sendOtpScreen(
     onLoginComplete = onLoginComplete,
     onSignUpComplete = onSignUpComplete,
   )
+}
+
+fun NavHostController.navigateToSendOtpScreen() = this.navigate(route = AuthRoute.SendOtp) {
+  popUpTo<MybraryRoute.Main> {
+    inclusive = true
+  }
 }
