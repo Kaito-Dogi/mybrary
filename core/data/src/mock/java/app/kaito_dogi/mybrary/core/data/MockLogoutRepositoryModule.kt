8992e6c244431a857cd3ca31b85e64d66826e9a9
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-03 00:07:25 +0900

    feat: Mock の実装を追加

---
+++ core/data/src/mock/java/app/kaito_dogi/mybrary/core/data/MockLogoutRepositoryModule.kt
@@ -0,0 +1,16 @@
+package app.kaito_dogi.mybrary.core.data
+
+import app.kaito_dogi.mybrary.core.domain.repository.LogoutRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface MockLogoutRepositoryModule {
+  @Singleton
+  @Binds
+  fun bindLogoutRepository(impl: MockLogoutRepository): LogoutRepository
+}
