f2620ac4b1086878953c3990a056f49d4e01af88
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-07 03:05:06 +0900

    feat: MybraryAnonApi で HCaptchaToken を使用

--- core/data/src/main/java/app/kaito_dogi/mybrary/core/data/repository/SignUpRepositoryImpl.kt
+++ core/data/src/main/java/app/kaito_dogi/mybrary/core/data/repository/SignUpRepositoryImpl.kt
@@ -3,6 +3,7 @@ package app.kaito_dogi.mybrary.core.data.repository
 import app.kaito_dogi.mybrary.core.api.mybrary.MybraryAnonApi
 import app.kaito_dogi.mybrary.core.common.coroutines.MybraryDispatcher
 import app.kaito_dogi.mybrary.core.common.coroutines.MybraryDispatchers
+import app.kaito_dogi.mybrary.core.domain.model.HCaptchaToken
 import app.kaito_dogi.mybrary.core.domain.repository.SignUpRepository
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -18,7 +19,7 @@ internal class SignUpRepositoryImpl @Inject constructor(
     TODO("Not yet implemented")
   }
 
-  override suspend fun anonymousSignUp() {
-    mybraryAnonApi.anonymousSignUp()
+  override suspend fun anonymousSignUp(hCaptchaToken: HCaptchaToken) {
+    mybraryAnonApi.anonymousSignUp(hCaptchaToken = hCaptchaToken.value)
   }
 }
--- core/domain/src/main/java/app/kaito_dogi/mybrary/core/domain/repository/SignUpRepository.kt
+++ core/domain/src/main/java/app/kaito_dogi/mybrary/core/domain/repository/SignUpRepository.kt
@@ -1,7 +1,9 @@
 package app.kaito_dogi.mybrary.core.domain.repository
 
+import app.kaito_dogi.mybrary.core.domain.model.HCaptchaToken
+
 interface SignUpRepository {
   suspend fun googleSignUp()
 
-  suspend fun anonymousSignUp()
+  suspend fun anonymousSignUp(hCaptchaToken: HCaptchaToken)
 }
