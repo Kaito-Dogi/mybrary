a036d31b1f233a71275770513142bea65f10486f
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-14 22:52:11 +0900

    feat: AuthRemoteDataSource を定義

--- core/data/src/main/java/app/kaito_dogi/mybrary/core/data/datasource/AuthRemoteDataSource.kt
+++ core/data/src/main/java/app/kaito_dogi/mybrary/core/data/datasource/AuthRemoteDataSource.kt
@@ -1,3 +1,20 @@
 package app.kaito_dogi.mybrary.core.data.datasource
 
-interface AuthRemoteDataSource
+interface AuthRemoteDataSource {
+  suspend fun sendOtp(
+    email: String,
+    captchaToken: String,
+  )
+
+  suspend fun verifyOtp(
+    email: String,
+    otp: String,
+    captchaToken: String,
+  )
+
+  suspend fun googleSignIn()
+
+  suspend fun anonymousSignIn(captchaToken: String)
+
+  suspend fun logout()
+}
