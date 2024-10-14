3f59bcb9ae4face72bdbdc5f545fdafe409845b5
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-14 23:18:30 +0900

    feat: MockAuthRepository を実装

--- core/repository-mock/src/main/java/app/kaito_dogi/mybrary/core/repository/mock/MockAuthRepository.kt
+++ core/repository-mock/src/main/java/app/kaito_dogi/mybrary/core/repository/mock/MockAuthRepository.kt
@@ -1,18 +1,35 @@
 package app.kaito_dogi.mybrary.core.repository.mock
 
-import app.kaito_dogi.mybrary.core.common.coroutines.MybraryDispatcher
-import app.kaito_dogi.mybrary.core.common.coroutines.MybraryDispatchers
+import app.kaito_dogi.mybrary.core.domain.model.HCaptchaToken
 import app.kaito_dogi.mybrary.core.domain.repository.AuthRepository
 import javax.inject.Inject
-import javax.inject.Singleton
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.delay
 
-@Singleton
-internal class MockAuthRepository @Inject constructor(
-  @MybraryDispatcher(MybraryDispatchers.Io) private val dispatcher: CoroutineDispatcher,
-) : AuthRepository {
-  override suspend fun hasSession(): Boolean = withContext(dispatcher) {
-    return@withContext false
+internal class MockAuthRepository @Inject constructor() : AuthRepository {
+  override suspend fun sendOtp(
+    email: String,
+    captchaToken: HCaptchaToken,
+  ) {
+    delay(1_000)
+  }
+
+  override suspend fun verifyOtp(
+    email: String,
+    otp: String,
+    captchaToken: HCaptchaToken,
+  ) {
+    delay(1_000)
+  }
+
+  override suspend fun googleSignIn() {
+    delay(1_000L)
+  }
+
+  override suspend fun anonymousSignIn(captchaToken: HCaptchaToken) {
+    delay(1_000L)
+  }
+
+  override suspend fun logout() {
+    delay(1_000)
   }
 }
