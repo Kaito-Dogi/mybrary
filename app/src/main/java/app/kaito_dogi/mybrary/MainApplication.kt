cc3fdfbcc2b47570a8e4579249c38bf144524a72
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-15 23:05:54 +0900

    style: MybraryApplication を MainApplication に修正

--- app/src/main/java/app/kaito_dogi/mybrary/MainApplication.kt
+++ app/src/main/java/app/kaito_dogi/mybrary/MainApplication.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-internal class MybraryApplication : Application()
+internal class MainApplication : Application()
