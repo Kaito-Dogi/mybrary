d42242e6f08364e64261bfa9117f9c338b805120
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-23 00:01:23 +0900

    feat: SignUpViewModel を実装

--- feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpUiEvent.kt
+++ feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpUiEvent.kt
@@ -1,6 +1,6 @@
 package app.kaito_dogi.mybrary.feature.signup
 
-sealed interface SignUpUiEvent {
+internal sealed interface SignUpUiEvent {
   data object OnSendOtp : SignUpUiEvent
   data object OnGoogleSignUp : SignUpUiEvent
   data object OnAnonymousSignUp : SignUpUiEvent
--- feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpUiState.kt
+++ feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpUiState.kt
@@ -1,15 +1,13 @@
 package app.kaito_dogi.mybrary.feature.signup
 
 import androidx.compose.runtime.Immutable
-import app.kaito_dogi.mybrary.core.ui.model.HCaptchaPurpose
 
 @Immutable
-internal class SignUpUiState(
+internal data class SignUpUiState(
   val email: String,
   val isOtpSending: Boolean,
   val isAnonymousSigningUp: Boolean,
   val isGoogleSigningUp: Boolean,
-  val hCaptchaPurpose: HCaptchaPurpose?,
 ) {
   companion object {
     val InitialValue = SignUpUiState(
@@ -17,9 +15,8 @@ internal class SignUpUiState(
       isOtpSending = false,
       isAnonymousSigningUp = false,
       isGoogleSigningUp = false,
-      hCaptchaPurpose = null,
     )
   }
 
-  val isHCaptchaVisible: Boolean = hCaptchaPurpose != null
+  val isHCaptchaVisible: Boolean = isOtpSending || isAnonymousSigningUp
 }
