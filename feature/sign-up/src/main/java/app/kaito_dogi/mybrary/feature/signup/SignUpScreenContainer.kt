99d5f07f5e95521ab4f656b7dce0808d26001c26
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-23 00:07:59 +0900

    feat: SignUpScreenContainer を実装

--- feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpScreenContainer.kt
+++ feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpScreenContainer.kt
@@ -1,17 +1,43 @@
 package app.kaito_dogi.mybrary.feature.signup
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.flowWithLifecycle
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @Composable
-internal fun SignUpScreenContainer() {
+internal fun SignUpScreenContainer(
+  viewModel: SignUpViewModel = hiltViewModel(),
+) {
+  val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+  val lifecycleOwner = LocalLifecycleOwner.current
+  LaunchedEffect(viewModel, lifecycleOwner) {
+    viewModel.uiEvent
+      .flowWithLifecycle(lifecycleOwner.lifecycle)
+      .onEach { uiEvent ->
+        when (uiEvent) {
+          SignUpUiEvent.OnAnonymousSignUp -> TODO()
+          SignUpUiEvent.OnGoogleSignUp -> TODO()
+          SignUpUiEvent.OnSendOtp -> TODO()
+        }
+      }
+      .launchIn(scope = this)
+  }
+
   SignUpScreen(
-    uiState = SignUpUiState.InitialValue,
-    onEmailChange = {},
-    onSendOtpClick = {},
-    onGoogleSignUpClick = {},
-    onAnonymousSignUpClick = {},
+    uiState = uiState,
+    onEmailChange = viewModel::onEmailChange,
+    onSendOtpClick = viewModel::onSendOtpClick,
+    onGoogleSignUpClick = viewModel::onGoogleSignUpClick,
+    onAnonymousSignUpClick = viewModel::onAnonymousSignUpClick,
     onNavigateToSignInClick = {},
-    onHCaptchaSuccess = {},
-    onHCaptchaFailure = {},
+    onHCaptchaSuccess = viewModel::onHCaptchaSuccess,
+    onHCaptchaFailure = viewModel::onHCaptchaFailure,
   )
 }
