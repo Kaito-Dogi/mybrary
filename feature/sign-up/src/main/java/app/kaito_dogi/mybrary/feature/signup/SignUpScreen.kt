56f6f0a7599fc73cf472e4756aa7348c0bb63758
Kaito-Dogi <kendobu0405@gmail.com>
2024-10-22 23:30:33 +0900

    feat: SignUpScreen を実装

---
+++ feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpScreen.kt
@@ -0,0 +1,138 @@
+package app.kaito_dogi.mybrary.feature.signup
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import app.kaito_dogi.core.hcaptcha.HCaptchaDialog
+import app.kaito_dogi.mybrary.core.common.model.CaptchaToken
+import app.kaito_dogi.mybrary.core.designsystem.component.PrimaryButton
+import app.kaito_dogi.mybrary.core.designsystem.component.SecondaryButton
+import app.kaito_dogi.mybrary.core.designsystem.ext.plus
+import app.kaito_dogi.mybrary.core.designsystem.theme.MybraryTheme
+import app.kaito_dogi.mybrary.core.ui.R
+import app.kaito_dogi.mybrary.core.ui.component.OrDivider
+import app.kaito_dogi.mybrary.core.ui.component.OtpSection
+import app.kaito_dogi.mybrary.core.ui.component.TermsOfUsePrivacyPolicyText
+import app.kaito_dogi.mybrary.feature.signup.component.NavigateToSignInSection
+
+@Composable
+internal fun SignUpScreen(
+  uiState: SignUpUiState,
+  onEmailChange: (String) -> Unit,
+  onSendOtpClick: () -> Unit,
+  onGoogleSignUpClick: () -> Unit,
+  onAnonymousSignUpClick: () -> Unit,
+  onNavigateToSignInClick: () -> Unit,
+  onHCaptchaSuccess: (CaptchaToken) -> Unit,
+  onHCaptchaFailure: () -> Unit,
+) {
+  Scaffold(
+    modifier = Modifier.fillMaxSize(),
+  ) { innerPadding ->
+    Column(
+      modifier = Modifier
+        .fillMaxSize()
+        .background(MybraryTheme.backgroundBrush)
+        .padding(
+          innerPadding.plus(
+            start = MybraryTheme.spaces.md,
+            end = MybraryTheme.spaces.md,
+            bottom = MybraryTheme.spaces.xl,
+          ),
+        ),
+    ) {
+      Text(
+        text = stringResource(id = R.string.send_otp_text_logo),
+        modifier = Modifier
+          .padding(vertical = MybraryTheme.spaces.xxxl)
+          .fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        style = MybraryTheme.typography.displayMedium,
+      )
+
+      OtpSection(
+        email = uiState.email,
+        onEmailChange = onEmailChange,
+        onSendOtpClick = onSendOtpClick,
+        isLoading = false,
+      )
+
+      OrDivider(
+        modifier = Modifier.padding(vertical = MybraryTheme.spaces.xl),
+      )
+
+      Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(MybraryTheme.spaces.xs),
+      ) {
+        SecondaryButton(
+          textResId = R.string.send_otp_text_sign_up_with_google,
+          onClick = onGoogleSignUpClick,
+          modifier = Modifier.fillMaxWidth(),
+          iconResId = R.drawable.icon_google,
+          altResId = R.string.send_otp_alt_sign_up_google,
+          iconTint = Color.Unspecified,
+          isLoading = uiState.isGoogleSigningUp,
+        )
+
+        PrimaryButton(
+          textResId = R.string.send_otp_text_sign_up_as_a_guest,
+          onClick = onAnonymousSignUpClick,
+          modifier = Modifier.fillMaxWidth(),
+          iconResId = R.drawable.icon_person,
+          altResId = R.string.send_otp_alt_sign_up_as_a_guest,
+          isLoading = uiState.isAnonymousSigningUp,
+        )
+
+        NavigateToSignInSection(
+          onClick = onNavigateToSignInClick,
+          modifier = Modifier
+            .padding(vertical = MybraryTheme.spaces.xxs)
+            .fillMaxWidth(),
+        )
+      }
+
+      Spacer(modifier = Modifier.weight(1f))
+
+      TermsOfUsePrivacyPolicyText(
+        modifier = Modifier.fillMaxWidth(),
+      )
+    }
+
+    if (uiState.isHCaptchaVisible) {
+      HCaptchaDialog(
+        onSuccess = onHCaptchaSuccess,
+        onFailure = onHCaptchaFailure,
+      )
+    }
+  }
+}
+
+@Preview
+@Composable
+private fun SignUpScreenPreview() {
+  MybraryTheme {
+    SignUpScreen(
+      uiState = SignUpUiState.InitialValue,
+      onEmailChange = {},
+      onSendOtpClick = {},
+      onGoogleSignUpClick = {},
+      onAnonymousSignUpClick = {},
+      onNavigateToSignInClick = {},
+      onHCaptchaSuccess = {},
+      onHCaptchaFailure = {},
+    )
+  }
+}
--- feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpScreenContainer.kt
+++ feature/sign-up/src/main/java/app/kaito_dogi/mybrary/feature/signup/SignUpScreenContainer.kt
@@ -4,4 +4,14 @@ import androidx.compose.runtime.Composable
 
 @Composable
 internal fun SignUpScreenContainer() {
+  SignUpScreen(
+    uiState = SignUpUiState.InitialValue,
+    onEmailChange = {},
+    onSendOtpClick = {},
+    onGoogleSignUpClick = {},
+    onAnonymousSignUpClick = {},
+    onNavigateToSignInClick = {},
+    onHCaptchaSuccess = {},
+    onHCaptchaFailure = {},
+  )
 }
